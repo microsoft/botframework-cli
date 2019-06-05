1c90c8540aa54c5d92c6b9a79e1f32d62b1372d0
Emilio Munoz <emmunozp@microsoft.com>
2019-06-05 09:50:07 -0700

    Flush telemetry client to avoid performance issues in cli

--- packages/command/src/command.ts
+++ packages/command/src/command.ts
@@ -7,7 +7,32 @@ const pjson = require('../package.json')
 export abstract class Command extends Base {
   base = `${pjson.name}@${pjson.version}`
 
-  TrackEvent(msg : string, properties?: { [key: string]: any }){
+  // Implement telemetry tracking in Command
+  async TrackEvent(msg : string, properties?: { [key: string]: any }){
     Telemetry.trackEvent(msg, properties);
+  }
+
+  async init(){ 
+    this.TrackEvent(this.id+'', {'flags' : this.getTelemetryProperties()})
+    super.init();
+  }
+
+  error(input: string | Error, options: {code?: string, exit: false}): void
+  error(input: string | Error, options?: {code?: string, exit?: number}): never
+  error(input: string | Error, options: {code?: string, exit?: number | false} = {}) {
+    this.TrackEvent(this.id+'', {'flags' : this.getTelemetryProperties(), 'error': input})
+    return super.error(input, options as any);
+  }
+
+  private getTelemetryProperties() : Array<string>{
+        // Retrieve flags from command
+        const {flags, argv} = this.parse(this.ctor)
+
+        // Iterate through flags and store only flags and not parameters or arguments to avoid instrumentation on user data
+        let properties : string [] = [];
+        for (let key in flags) {
+          properties.push(key);
+        }
+        return properties;
   }
 }
--- packages/command/src/telemetry.ts
+++ packages/command/src/telemetry.ts
@@ -5,16 +5,17 @@ const INSTRUMENTATION_KEY = '12ca0fd4-de70-420e-aa46-3ec7f6cd040d';
 class Telemetry{
   private static _client: AppInsights.TelemetryClient;
 
-  public static trackEvent(name: string, properties?: { [key: string]: any }): void {
+  public static trackEvent(name: string, properties?: { [key: string]: any }): void{
     if (!this._client) {
       this.startup();
     }
     try {
       this._client.trackEvent({ name, properties });
+      // TODO: Check is flush is better than reducing the Batch interval
+      //this._client.flush();
     } catch (e) {
       // swallow the exception; we don't want to crash the app
       // on a failed attempt to collect usage data
-      console.log(e)
     }
   }
 
@@ -31,6 +32,7 @@ class Telemetry{
         // Fix for zonejs / restify conflict (https://github.com/Microsoft/ApplicationInsights-node.js/issues/460)
         .setAutoDependencyCorrelation(false);
       // do not collect the user's machine name
+      AppInsights.defaultClient.config.maxBatchIntervalMs = 100;
       AppInsights.defaultClient.context.tags[AppInsights.defaultClient.context.keys.cloudRoleInstance] = '';
       AppInsights.start();
 
