9305ffc3e98ade75a8050cde857f80a6f2aa61e9
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-06-20 11:08:04 -0700

    Deleting lib files

--- packages/command/lib/telemetry.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Telemetry {
-    private static _client;
-    static trackEvent(name: string, properties?: {
-        [key: string]: any;
-    }): void;
-    static flushTelemetry(): void;
-    private static startup;
-}
-export default Telemetry;
