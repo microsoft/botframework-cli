f72fd83752add6088063cface3326932af01115b
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-07-11 11:03:57 -0700

    Adding renamed file

---
+++ packages/cli/src/hooks/init/configtelemetry.ts
@@ -0,0 +1,30 @@
+import {Hook} from '@oclif/config'
+import cli from 'cli-ux'
+import * as fs from 'fs-extra'
+const path = require('path')
+const chalk = require('chalk')
+
+const hook: Hook<'init'> = async function () {
+  try {
+    if (this.config.pjson.telemetry === null) {
+      const disableTelemetry = await cli.prompt(chalk.red('Telemetry is enabled. Would you like to opt out? (Y/N)'))
+      if (disableTelemetry === 'Y' || disableTelemetry === 'y') {
+        this.config.pjson.telemetry = false
+        this.log(chalk.blue('Telemetry has been disabled.'))
+      } else {
+        this.config.pjson.telemetry = true
+        this.log(chalk.blue('Telemetry will remain enabled'))
+      }
+      const pathToJson = path.resolve(__dirname, '../../../package.json')
+      const userConfig = await fs.readJSON(pathToJson)
+      userConfig.telemetry = this.config.pjson.telemetry
+      await fs.writeFile(pathToJson, JSON.stringify(userConfig, null, 2))
+    }
+  /* tslint:disable:no-unused */
+  } catch (err) {
+      // swallow the exception; we don't want to crash the app
+      // on a failed attempt to set telemetry
+  }
+}
+
+export default hook
