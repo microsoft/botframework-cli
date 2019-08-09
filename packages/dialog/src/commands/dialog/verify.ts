32eafcee08c87fe8bb81d2839147b7981546d06a
Emilio Munoz <emmunozp@microsoft.com>
2019-08-09 11:18:35 -0700

    Revert "merge"
    This reverts commit e7691e4250b7797504ad0969dd58dce74e5541cf, reversing
    changes made to cc1b974639c8f8cb72e6d2fe31c8cdd08533ea4f.

--- packages/dialog/src/commands/dialog/verify.ts
+++ packages/dialog/src/commands/dialog/verify.ts
@@ -1,73 +1,25 @@
-import { Command, flags } from '@oclif/command';
-import { Definition, DialogTracker, SchemaTracker } from '../../library/dialogTracker';
-// import * as process from 'process';
+import {Command, flags} from '@oclif/command'
 
 export default class DialogVerify extends Command {
-    static flags = {
-        help: flags.help({ char: 'h' }),
-        write: flags.string({ char: 'w', description: 'Write dialog lg information into .lg files where <write> is an .lg file with no locale.' }),
+  static description = 'describe the command here'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({char: 'n', description: 'name to print'}),
+    // flag with no value (-f, --force)
+    force: flags.boolean({char: 'f'}),
+  }
+
+  static args = [{name: 'file'}]
+
+  async run() {
+    const {args, flags} = this.parse(DialogVerify)
+
+    const name = flags.name || 'world'
+    this.log(`hello ${name}`)
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`)
     }
-
-    async run() {
-        const { argv, flags } = this.parse(DialogVerify)
-
-
-        const schema = new SchemaTracker();
-        const tracker = new DialogTracker(schema);
-
-        await tracker.addDialogFiles(argv);
-
-        if (tracker.dialogs.length == 0) {
-            this._help();
-        } else {
-            for (let dialog of tracker.dialogs) {
-                if (dialog.errors.length == 0) {
-                    this.log(`Processed ${dialog}`);
-                } else {
-                    this.error(`Errors processing ${dialog}`);
-                    for (let error of dialog.errors) {
-                        this.error(`  ${error.message}`);
-                    }
-                }
-            }
-
-            for (let defs of tracker.multipleDefinitions()) {
-                let def = (<Definition[]>defs)[0];
-                this.error(`Multiple definitions for ${def} ${def.usedByString()}`);
-                for (let def of defs) {
-                    this.error(`  ${def.pathString()}`);
-                }
-            }
-
-            for (let def of tracker.missingDefinitions()) {
-                this.error(`Missing definition for ${def} ${def.usedByString()}`);
-            }
-
-            for (let def of tracker.missingTypes) {
-                this.error(`Missing $type for ${def}`);
-            }
-
-            for (let def of tracker.unusedIDs()) {
-                this.warn(`Unused id ${def}`);
-            }
-
-            for (let [type, definitions] of tracker.typeToDef) {
-                this.log(`Instances of ${type}`);
-                for (let def of definitions) {
-                    this.log(`  ${def.locatorString()}`);
-                }
-            }
-
-            if (flags.write) {
-                tracker.writeLG(flags.write, (msg) => this.log(msg));
-            }
-        }
-    }
-
+  }
 }
-
-// interface IPackage {
-//     version: string;
-//     engines: { node: string };
-// }
-
