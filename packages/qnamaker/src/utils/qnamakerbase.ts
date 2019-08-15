cb94a6577dc3acdbecb4264810b75e86dc4a4311
Emilio Munoz <emmunozp@microsoft.com>
2019-08-15 12:44:09 -0700

    Adding preview message

--- packages/qnamaker/src/utils/qnamakerbase.ts
+++ packages/qnamaker/src/utils/qnamakerbase.ts
@@ -7,26 +7,24 @@ const {ServiceBase} = require('./../../utils/api/serviceBase')
 export async function processInputs(flags: any, payload: any, verb: string, target: string | undefined, configfile: string) {
   let result: Inputs = {}
   const config = await qnaconfig.composeConfig(flags, configfile)
-
   let service = {}
-  if (flags.stdin) {
-    let json = await stdin()
-    service = await JSON.parse(json)
-  }
-
-  qnaconfig.buildConfig(flags, service, config)
-  ServiceBase.config = flags
-
+  
   try {
+    if (flags.stdin) {
+      let json = await stdin()
+      service = await JSON.parse(json)
+    }
+    qnaconfig.buildConfig(flags, service, config)
+    ServiceBase.config = flags
     await qnaconfig.validateConfig(flags)
+    const serviceManifest = srvMan.getServiceManifest(verb, target, payload)
+    result.requestBody = await srvMan.validateArguments(serviceManifest, flags)
+    result.config = config
+    result.serviceManifest = serviceManifest
   } catch (e) {
     throw new CLIError(e.message)
   }
 
-  const serviceManifest = srvMan.getServiceManifest(verb, target, payload)
-  result.requestBody = await srvMan.validateArguments(serviceManifest, flags)
-  result.config = config
-  result.serviceManifest = serviceManifest
   return result
 }
 
