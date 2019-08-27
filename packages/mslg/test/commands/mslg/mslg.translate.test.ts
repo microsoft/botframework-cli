8ef87178834ce989e7910d557cef248fa0cf72dc
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-08-27 11:18:55 -0700

    Mocking translator calls in unit tests

--- packages/mslg/test/commands/mslg/mslg.translate.test.ts
+++ packages/mslg/test/commands/mslg/mslg.translate.test.ts
@@ -3,22 +3,24 @@ const fs = require('fs')
 const path = require('path');
 const chai = require('chai')
 const assert = chai.assert
+const nock = require('nock')
 const txtfile = require('read-text-file')
 const rimraf = require('rimraf')
 const dotenv = require('dotenv');
-const ENV_FILE = path.join(__dirname, '../../../.env');
+const testDir = 'test/testOutput';
+const translateMockResponse = require('../../fixtures/translator/zh-Hans/translateMockResponse.json');
+
 dotenv.config({ path: '.env' });
 const TRANSLATE_KEY = process.env.TRANSLATE_KEY
-const testDir = 'test/testOutput';
 
-function compareFiles(actualPath: string, expectedPath: string) {
-  let expected = fs.existsSync(actualPath)
+function compareFiles(actualPath: string, fixturesPath: string) {
+  let fixtures = fs.existsSync(actualPath)
     ? txtfile.readSync(actualPath)
     : actualPath
-  let actual = fs.existsSync(expectedPath)
-    ? txtfile.readSync(expectedPath)
-    : 'expectedPath'
-  assert.deepEqual(actual.split(/\r?\n/), expected.split(/\r?\n/))
+  let actual = fs.existsSync(fixturesPath)
+    ? txtfile.readSync(fixturesPath)
+    : 'fixturesPath'
+  assert.deepEqual(actual.split(/\r?\n/), fixtures.split(/\r?\n/))
 }
 
 if (TRANSLATE_KEY) {
@@ -29,11 +31,18 @@ if (TRANSLATE_KEY) {
         fs.mkdirSync(testDir);
       }
     });
+
+    beforeEach(() => {
+      nock('https://api.cognitive.microsofttranslator.com')
+        .persist()
+        .post(/.*/)
+        .reply(200, translateMockResponse)
+    })
     
     test
       .stdout()
       .stderr()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/exceptionExamples/InvalidTemplateName.lg', '-o', 'test/testOutput', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/exceptionExamples/InvalidTemplateName.lg', '--out_folder', 'test/testOutput', '-c', '--verbose'])
       .it('should output an error for invalid token', ctx => {
         expect(ctx.stderr).to.contain('')
       })
@@ -41,7 +50,7 @@ if (TRANSLATE_KEY) {
     test
       .stdout()
       .stderr()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/exceptionExamples/completelyEmptyFile.lg', '-o', 'test/testOutput', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/exceptionExamples/completelyEmptyFile.lg', '--out_folder', 'test/testOutput', '-c', '--verbose'])
       .it('should output an error for invalid file type', ctx => {
         expect(ctx.stderr).to.contain('Encoding not recognized')
       })
@@ -49,14 +58,14 @@ if (TRANSLATE_KEY) {
     test
       .stdout()
       .stderr()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/xyz/completelyEmptyFile.lg', '-o', 'test/testOutput', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/xyz/completelyEmptyFile.lg', '--out_folder', 'test/testOutput', '-c', '--verbose'])
       .it('should output an error for folder does not exist', ctx => {
         expect(ctx.stderr).to.contain('unable to open file')
       })
 
     test
       .stdout()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/validExamples/translator.lg', '-o', 'test/testOutput', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/validExamples/translator.lg', '--out_folder', 'test/testOutput', '-c', '--verbose'])
       .it('should translate a specific lg file', ctx => {
         expect(ctx.stdout).to.contain('Parsing file: ')
       })
@@ -64,27 +73,23 @@ if (TRANSLATE_KEY) {
     test
       .stdout()
       .stderr()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/validExamples/translator.lg', '-o', '../testOutput', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '--in', 'examples/validExamples/translator.lg', '--out_folder', '../testOutput', '-c', '--verbose'])
       .it('should translate a specific lg file with outfolder relative path specified', ctx => {
         expect(ctx.stderr).to.contain('output folder')
       })
 
     test
       .stdout()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '-l', 'examples/validExamples', '-o', 'test/testOutput', '-s', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '-l', 'examples/validExamples', '--out_folder', 'test/testOutput', '-s', '-c', '--verbose'])
       .it('should translate in subfolder', ctx => {
         expect(ctx.stdout).to.contain('Parsing file: ')
       })
 
     test
       .stdout()
-      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '-l', 'examples/validExamples', '--out_folder', 'test/testOutput', '-s', '-c', '--verbose'])
+      .command(['mslg:translate', '-k', `${TRANSLATE_KEY}`, '-t', 'zh-Hans', '-l', 'examples/validExamples/batch', '--out_folder', 'test/testOutput', '-s', '-c', '--verbose'])
       .it('should translate all files from a specific folder', ctx => {
-        compareFiles('test/testOutput/zh-Hans/subSimple.lg', 'test/expected/zh-Hans/subSimple.lg');
-        compareFiles('test/testOutput/zh-Hans/simple.lg', 'test/expected/zh-Hans/simple.lg');
-        compareFiles('test/testOutput/zh-Hans/simple2.lg', 'test/expected/zh-Hans/simple2.lg');
-        compareFiles('test/testOutput/zh-Hans/simpleWithVariables.lg', 'test/expected/zh-Hans/simpleWithVariables.lg');
-        compareFiles('test/testOutput/zh-Hans/translator.lg', 'test/expected/zh-Hans/translator.lg');
+        compareFiles('test/testOutput/zh-Hans/translator.lg', 'test/fixtures/translator/zh-Hans/translator.lg');
       })
 
     afterEach(done => {
