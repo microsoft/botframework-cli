e767c12d4d7b333c3a80d60331a28d51f99dde82
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-09-06 15:21:58 -0700

    Flags refactor & tests passing

--- packages/chatdown/src/commands/chatdown.ts
+++ packages/chatdown/src/commands/chatdown.ts
@@ -12,14 +12,13 @@ export default class Chatdown extends Command {
 
   static examples = [`
   $ bf chatdown
-  $ bf chatdown --chat=./path/to/file/sample.chat
+  $ bf chatdown --in=./path/to/file/sample.chat
   $ bf chatdown -f ./test/utils/*.sample.chat -o ./
   $ (echo user=Joe && [ConversationUpdate=MembersAdded=Joe]) | bf chatdown --static`]
 
   static flags = {
-    chat: flags.string({char: 'c', description: 'The path of the chat file to be parsed. If omitted, stdin will be used.'}),
-    folder: flags.string({char: 'f', description: 'Path to directory and/or all subdirectories containing chat files to be processed all at once, ex. ./**/*.chat. If an output directory is not present (-o), it will default the output to the current working directory. '}),
-    out_folder: flags.string({char: 'o', description: 'Path to the directory where the output of the multiple chat file processing (-f) will be placed.'}),
+    in: flags.string({char: 'i', description: 'The path of the chat file or directory to be parsed. A glob expression may be passed containing chat files to be processed all at once, ex. ./**/*.chat. If flag is omitted, stdin will be used. If an output directory is not present (-o), it will default the output to the current working directory.'}),
+    out: flags.string({char: 'o', description: 'Path to the directory where the output of the multiple chat file processing (-f) will be placed.'}),
     static: flags.boolean({char: 's', description: 'Use static timestamps when generating timestamps on activities.'}),
     prefix: flags.boolean({char: 'p', description: 'Prefix stdout with package name.'}),
     help: flags.help({char: 'h', description: 'Chatdown command help'})
@@ -29,6 +28,8 @@ export default class Chatdown extends Command {
     try {
       const {flags} = this.parse(Chatdown)
 
+      let inputIsDirectory = flags.in ? (flags.in.includes('*') || this.isDir(flags.in)) : false
+
       if (flags.prefix) {
         const pkgName = this.config.name
         intercept(function (txt: any) {
@@ -36,37 +37,43 @@ export default class Chatdown extends Command {
         })
       }
 
-      if (flags.folder) {
-        let inputDir = flags.folder.trim()
-        let outputDir = (flags.out_folder) ? flags.out_folder.trim() : './'
+      if (inputIsDirectory) {
+        let inputDir = flags.in ? flags.in.trim() : ''
+        let outputDir = (flags.out) ? flags.out.trim() : './'
         if (outputDir.substr(0, 2) === './') {
           outputDir = path.resolve(process.cwd(), outputDir.substr(2))
         }
         const len = await this.processFiles(inputDir, outputDir)
         if (len === 0) {
-          throw new CLIError('No chat files found at: ' + flags.folder)
+          throw new CLIError('No chat files found at: ' + flags.in)
         }
         this.log(chalk.green(`Successfully wrote ${len} files\n`))
         return
+      } else {
+        const fileContents = await this.getInput(flags.in)
+        if (fileContents) {
+          const activities = await chatdown(fileContents, flags)
+          const writeConfirmation = await this.writeOut(activities)
+          /* tslint:disable:strict-type-predicates */
+          if (typeof writeConfirmation === 'string') {
+            process.stdout.write(`${chalk.green('Successfully wrote file:')}  ${writeConfirmation}\n`)
+          }
+        } else {
+          return this._help()
+        }
       }
 
-      let fileContents = await this.getInput(flags.chat)
-      if (fileContents) {
-        const activities = await chatdown(fileContents, flags)
-        const writeConfirmation = await this.writeOut(activities)
-        /* tslint:disable:strict-type-predicates */
-        if (typeof writeConfirmation === 'string') {
-          process.stdout.write(`${chalk.green('Successfully wrote file:')}  ${writeConfirmation}\n`)
-        }
-      } else {
-        return this._help()
-      }
     } catch (err) {
       if (err.message.match(/Malformed configurations options detected/)) {
         throw new CLIError(err.message)
       }
       throw err
     }
+  }
+
+  private readonly isDir = (path: string) => {
+    const stats = fs.statSync(path)
+    return stats.isDirectory()
   }
 
   private async getInput(args: any) {
