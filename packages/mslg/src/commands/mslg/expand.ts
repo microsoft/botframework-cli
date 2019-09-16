51bc3ed3cdc92f402e156c468e17e06615a19d94
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-09-16 13:01:05 -0700

    Refactor parse and expand

--- packages/mslg/src/commands/mslg/expand.ts
+++ packages/mslg/src/commands/mslg/expand.ts
@@ -1,11 +1,22 @@
 import {CLIError, Command, flags} from '@microsoft/bf-cli-command'
-const {Expander} = require('../../utils/expander')
+import {MSLGTool} from 'botbuilder-lg'
+import * as chalk from 'chalk'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import * as txtfile from 'read-text-file'
+
+import {ErrorType} from '../../utils/helpers'
+
+const readlineSync = require('readline-sync')
 
 export default class MslgExpand extends Command {
-  static description = 'Expand one or all templates in a .lg file or an inline expression.'
+  static description =
+    'Expand one or all templates in a .lg file or an inline expression.'
 
-  static examples = [`
-  $ bf mslg expand --in examples/validExamples/simple.lg -t FinalGreeting`]
+  static examples = [
+    `
+  $ bf mslg expand --in examples/validExamples/simple.lg -t FinalGreeting`
+  ]
 
   static flags = {
     in: flags.string({required: true, description: 'The .lg file to expand'}),
@@ -17,13 +28,220 @@ export default class MslgExpand extends Command {
     help: flags.help({description: 'Output usage information.'})
   }
 
+  private readonly tool: MSLGTool = new MSLGTool()
+
   async run() {
     try {
       const {flags} = this.parse(MslgExpand)
-      const expander: any = new Expander()
-      await expander.Expand(flags)
+
+      let fileToExpand: any
+      if (flags.in) {
+        fileToExpand = flags.in
+      }
+
+      let errors: string[] = []
+      errors = this.parseFile(fileToExpand, flags.inline)
+
+      if (
+        errors.filter(error => error.startsWith(ErrorType.Error)).length > 0
+      ) {
+        throw new Error('parsing lg file or inline expression failed.')
+      }
+
+      let templatesName: string[] = []
+      if (flags.template) {
+        templatesName.push(flags.template)
+      }
+
+      if (flags.all) {
+        templatesName = Array.from(
+          new Set(
+            templatesName.concat(
+              this.getTemplatesName(this.tool.CollatedTemplates)
+            )
+          )
+        )
+      }
+
+      if (flags.inline) {
+        templatesName.push('__temp__')
+      }
+
+      let expandedTemplates: Map<string, string[]> = new Map<string, string[]>()
+      let variablesValue: Map<string, any>
+      let userInputValues: Map<string, any> = new Map<string, any>()
+      for (const templateName of templatesName) {
+        const expectedVariables = this.tool.GetTemplateVariables(templateName)
+        variablesValue = this.getVariableValues(
+          flags.testInput,
+          expectedVariables,
+          userInputValues
+        )
+        for (const variableValue of variablesValue) {
+          if (variableValue[1] === undefined) {
+            if (flags.interactive) {
+              let value = readlineSync.question(
+                `Please enter variable value of ${variableValue[0]} in template ${templateName}: `
+              )
+              let valueObj: any
+              try {
+                valueObj = JSON.parse(value)
+              } catch {
+                valueObj = value
+              }
+
+              variablesValue.set(variableValue[0], valueObj)
+              userInputValues.set(variableValue[0], valueObj)
+            }
+          }
+        }
+
+        const variableObj: any = this.generateVariableObj(variablesValue)
+        const expandedTemplate: string[] = this.tool.ExpandTemplate(
+          templateName,
+          variableObj
+        )
+        expandedTemplates.set(templateName, expandedTemplate)
+      }
+
+      if (!expandedTemplates || expandedTemplates.size === 0) {
+        throw new Error('expanding templates or inline expression failed')
+      }
+
+      let expandedTemplatesFile: string = this.generateExpandedTemplatesFile(
+        expandedTemplates
+      )
+
+      let fileName: string = flags.in
+      if (!fileName) {
+        expandedTemplatesFile = expandedTemplatesFile.replace(
+          '# __temp__\n- ',
+          ''
+        )
+      }
+
+      process.stdout.write(expandedTemplatesFile + '\n')
     } catch (error) {
       throw new CLIError(error.message)
     }
+  }
+
+  private parseFile(fileName: string, inlineExpression?: any): string[] {
+    let fileContent = ''
+    let filePath = ''
+    if (fileName) {
+      if (!fs.existsSync(path.resolve(fileName))) {
+        throw new Error('unable to open file: ' + fileName)
+      }
+
+      fileContent = txtfile.readSync(fileName)
+      if (!fileContent) {
+        throw new Error('unable to read file: ' + fileName)
+      }
+
+      filePath = path.resolve(fileName)
+    }
+
+    if (inlineExpression) {
+      const fakeTemplateId = '__temp__'
+      const wrappedStr = `\n# ${fakeTemplateId} \r\n - ${inlineExpression}`
+      fileContent += wrappedStr
+      filePath = path.resolve('./')
+    }
+
+    const errors: string[] = this.tool.ValidateFile(fileContent, filePath)
+    if (errors.length > 0) {
+      errors.forEach(error => {
+        if (error.startsWith(ErrorType.Error)) {
+          process.stderr.write(chalk.default.redBright(error + '\n'))
+        } else {
+          process.stdout.write(chalk.default.yellowBright(error + '\n'))
+        }
+      })
+    }
+
+    return errors
+  }
+
+  private generateExpandedTemplatesFile(
+    expandedTemplates: Map<string, string[]>
+  ): string {
+    let result = ''
+    for (const template of expandedTemplates) {
+      result += '# ' + template[0] + '\n'
+      if (template[1] instanceof Array) {
+        template[1].forEach(templateStr => {
+          result += '- ' + templateStr + '\n'
+        })
+      } else {
+        throw new Error('generating expanded lg file failed')
+      }
+
+      result += '\n'
+    }
+
+    return result
+  }
+
+  private getTemplatesName(collatedTemplates: Map<string, any>): string[] {
+    let result: string[] = []
+    for (const template of collatedTemplates) {
+      result.push(template[0])
+    }
+
+    return result
+  }
+
+  private getVariableValues(
+    testFileName: string | undefined,
+    expectedVariables: string[],
+    userInputValues: Map<string, any>
+  ): Map<string, any> {
+    let result: Map<string, any> = new Map<string, any>()
+    let variablesObj: any
+    if (testFileName) {
+      const filePath: string = path.resolve(testFileName)
+      if (!fs.existsSync(filePath)) {
+        throw new Error('unable to open file: ' + filePath)
+      }
+
+      let fileContent = txtfile.readSync(filePath)
+      if (!fileContent) {
+        throw new Error('unable to read file: ' + filePath)
+      }
+
+      variablesObj = JSON.parse(fileContent)
+    }
+
+    if (expectedVariables) {
+      for (const variable of expectedVariables) {
+        if (
+          variablesObj &&
+          variablesObj[variable]
+        ) {
+          result.set(variable, variablesObj[variable])
+        } else if (
+          userInputValues &&
+          userInputValues.has(variable)
+        ) {
+          result.set(variable, userInputValues.get(variable))
+        } else {
+          result.set(variable, undefined)
+        }
+      }
+    }
+
+    return result
+  }
+
+  private generateVariableObj(variablesValue: Map<string, any>): any {
+    let result: any = {}
+    if (variablesValue) {
+      for (const variable of variablesValue) {
+        result[variable[0]] = variable[1]
+      }
+    }
+
+    return result
   }
 }
--- packages/mslg/src/commands/mslg/parse.ts
+++ packages/mslg/src/commands/mslg/parse.ts
@@ -1,15 +1,26 @@
 import {CLIError, Command, flags} from '@microsoft/bf-cli-command'
-const {Parser} = require('../../utils/parser')
+import {MSLGTool} from 'botbuilder-lg'
+import * as chalk from 'chalk'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import * as txtfile from 'read-text-file'
+
+import {ErrorType, helpers} from '../../utils/helpers'
+
+const readlineSync = require('readline-sync')
 
 export default class MslgParse extends Command {
-  static description = 'Parse any provided .lg file and collate them into a single lg file.'
+  static description =
+    'Parse any provided .lg file and collate them into a single lg file.'
 
-  static examples = [`
+  static examples = [
+    `
   $ bf mslg parse --in examples/exceptionExamples/EmptyTemplate.lg
-  $ bf mslg parse -l examples/validExamples -s --out finalResult -c`]
+  $ bf mslg parse -l examples/validExamples -s --out finalResult -c`
+  ]
 
   static flags = {
-    in: flags.string({description: 'The .lg file to parse'}),
+    in: flags.string({char: 'i', description: 'The .lg file to parse'}),
     lg_folder: flags.string({description: 'Folder that has the .lg file. By default mslg will only look at the current folder. To look at all subfolders, include -s.'}),
     subfolder: flags.boolean({description: 'Include sub-folders as well when looking for .lg files.'}),
     out: flags.string({description: 'Output .lg file name.'}),
@@ -21,13 +32,208 @@ export default class MslgParse extends Command {
     help: flags.help({description: 'Output usage information.'})
   }
 
+  private readonly tool: MSLGTool = new MSLGTool()
+
   async run() {
     try {
       const {flags} = this.parse(MslgParse)
-      const parser: any = new Parser()
-      await parser.Parser(flags)
+
+      let filesToParse: any[] = []
+      let folderStat: any
+      if (flags.in) {
+        filesToParse.push(flags.in)
+      }
+
+      if (flags.lg_folder) {
+        try {
+          folderStat = fs.statSync(flags.lg_folder)
+        } catch (err) {
+          this.log(err)
+          throw new Error(flags.lg_folder + ' is not a folder or does not exist')
+        }
+        if (!folderStat.isDirectory()) {
+          throw new Error(flags.lg_folder + ' is not a folder or does not exist')
+        }
+        if (flags.subfolder) {
+          filesToParse = helpers.findLGFiles(flags.lg_folder, true)
+        } else {
+          filesToParse = helpers.findLGFiles(flags.lg_folder, false)
+        }
+        if (filesToParse.length === 0) {
+          throw new Error('no .lg files found in the specified folder.')
+        }
+      }
+
+      let errors: string[] = []
+
+      while (filesToParse.length > 0) {
+        let file = filesToParse[0]
+        const parseRes = this.parseFile(file, flags.verbose)
+        errors = errors.concat(parseRes)
+        filesToParse.splice(0, 1)
+      }
+
+      if (flags.stdin) {
+        let parsedJsonFromStdin
+        try {
+          let value = readlineSync.question(
+            'Please enter the lg file content: '
+          )
+          parsedJsonFromStdin = JSON.parse(value)
+        } catch (err) {
+          throw new Error(
+            `unable to parse stdin as JSON! \n\n ${JSON.stringify(
+              err,
+              null,
+              2
+            )}\n\n`
+          )
+        }
+
+        errors = errors.concat(
+          this.parseStream(parsedJsonFromStdin, flags.verbose)
+        )
+      }
+
+      if (
+        errors.filter(error => error.startsWith(ErrorType.Error)).length > 0
+      ) {
+        throw new Error('parsing lg files failed.')
+      }
+
+      let fileName: string
+      if (flags.out) {
+        fileName = flags.out + '_mslg.lg'
+      } else if (flags.in) {
+        if (!path.isAbsolute(flags.in)) {
+          fileName = path.resolve('', flags.in)
+        } else {
+          fileName = flags.in
+        }
+
+        let fileNameSegment = fileName.split('\\').pop()
+        fileName = fileNameSegment
+          ? fileNameSegment.replace('.lg', '') + '_mslg.lg'
+          : ''
+      } else {
+        if (flags.lg_folder && !path.isAbsolute(flags.lg_folder)) {
+          fileName = path.resolve('', flags.lg_folder)
+        } else {
+          fileName = flags.lg_folder ? flags.lg_folder : ''
+        }
+
+        fileName = fileName.split('\\').pop() + '_mslg.lg'
+      }
+
+      let outFolder: string = process.cwd()
+      if (flags.out_folder) {
+        if (path.isAbsolute(flags.out_folder)) {
+          outFolder = flags.out_folder
+        } else {
+          outFolder = path.resolve('', flags.out_folder)
+        }
+
+        if (!fs.existsSync(outFolder)) {
+          throw new Error('output folder ' + outFolder + ' does not exist')
+        }
+      }
+
+      if (this.tool.CollationMessages.length > 0) {
+        this.tool.CollationMessages.forEach(error => {
+          if (error.startsWith(ErrorType.Error)) {
+            process.stderr.write(chalk.default.redBright(error + '\n'))
+          } else {
+            process.stdout.write(chalk.default.yellowBright(error + '\n'))
+          }
+        })
+
+        throw new Error('collating lg files failed.' + '\n')
+      } else {
+        if (
+          !flags.collate &&
+          this.tool.NameCollisions.length > 0
+        ) {
+          throw new Error(
+            'below template names are defined in multiple files: ' +
+              this.tool.NameCollisions.toString()
+          )
+        } else {
+          const mergedLgFileContent = this.tool.CollateTemplates()
+          if (!mergedLgFileContent) {
+            throw new Error('generating collated lg file failed.')
+          }
+          const filePath = outFolder + '\\' + fileName
+          if (fs.existsSync(filePath)) {
+            throw new Error(
+              `a file named ${fileName} already exists in the folder ${outFolder}.`
+            )
+          } else {
+            if (flags.stdout) {
+              process.stdout.write(
+                chalk.default.whiteBright(mergedLgFileContent)
+              )
+            } else {
+              fs.writeFileSync(filePath, mergedLgFileContent)
+              process.stdout.write(
+                chalk.default.whiteBright(
+                  `Collated lg file is generated here: ${filePath}.\n`
+                )
+              )
+            }
+          }
+        }
+      }
     } catch (error) {
       throw new CLIError(error.message)
     }
+  }
+
+  private parseFile(fileName: string, verbose: boolean): string[] {
+    if (!fs.existsSync(path.resolve(fileName))) {
+      throw new Error('unable to open file: ' + fileName)
+    }
+
+    let fileContent = txtfile.readSync(fileName)
+    if (!fileContent) {
+      throw new Error('unable to read file: ' + fileName)
+    }
+
+    if (verbose)
+      process.stdout.write(
+        chalk.default.whiteBright('Parsing file: ' + fileName + '\n')
+      )
+
+    const errors: string[] = this.tool.ValidateFile(
+      fileContent,
+      path.resolve(fileName)
+    )
+    if (errors.length > 0) {
+      errors.forEach(error => {
+        if (error.startsWith(ErrorType.Error)) {
+          process.stderr.write(chalk.default.redBright(error + '\n'))
+        } else {
+          process.stdout.write(chalk.default.yellowBright(error + '\n'))
+        }
+      })
+    }
+
+    return errors
+  }
+
+  private parseStream(fileContent: string, verbose: boolean): string[] {
+    if (verbose)
+      process.stdout.write(chalk.default.whiteBright('Parsing from stdin.\n'))
+
+    const errors: string[] = this.tool.ValidateFile(
+      fileContent,
+      path.resolve('./')
+    )
+    if (errors.length > 0) {
+      errors.forEach(error => {
+        process.stderr.write(chalk.default.redBright(error + '\n'))
+      })
+    }
+
+    return errors
   }
 }
