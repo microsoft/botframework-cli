5106ae8d1e50dc9369a2a4ad30c5c81b724ed403
Emilio Munoz <emmunozp@microsoft.com>
2019-09-16 12:49:45 -0700

    Reducing telemetry config logic

--- packages/config/src/commands/config/set/telemetry.ts
+++ packages/config/src/commands/config/set/telemetry.ts
@@ -6,19 +6,13 @@ export default class ConfigSetTelemetry extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
-    disable: flags.boolean({char: 'd', description: 'Disable tlemetry'})
+    disable: flags.boolean({char: 'd', description: 'Disable tlemetry', default: false})
   }
 
   async run() {
     const {flags} = this.parse(ConfigSetTelemetry)
     let userConfig: Config = await getConfigFile(this.config.configDir)
-
-    if (flags.disable) {
-      userConfig.telemetry = false
-    } else {
-      userConfig.telemetry = true
-    }
-
+    userConfig.telemetry = !flags.disable
     await writeConfigFile(this.config.configDir, userConfig)
     this.log('Telemetry set to ' + flags.disable)
   }
