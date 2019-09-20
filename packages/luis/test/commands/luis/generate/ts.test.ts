53283b79402d5cd7f97c55bf36701f7411ea7b60
Axel Suarez <axsuarez@microsoft.com>
2019-09-20 16:32:43 -0700

    remove default tests

--- packages/luis/test/commands/luis/generate/ts.test.ts
+++ packages/luis/test/commands/luis/generate/ts.test.ts
@@ -1,17 +1,2 @@
-import {expect, test} from '@oclif/test'
-
 describe('luis:generate:ts', () => {
-  test
-    .stdout()
-    .command(['luis:generate:ts'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
-
-  test
-    .stdout()
-    .command(['luis:generate:ts', '--name', 'jeff'])
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
 })
