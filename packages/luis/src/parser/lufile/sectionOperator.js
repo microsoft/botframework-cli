62ec05692ab1765e1d847d895dacd30f265f30c6
hond <hond@microsoft.com>
2019-10-09 18:16:17 +0800

    add more tests

---
+++ packages/luis/src/parser/lufile/sectionOperator.js
@@ -0,0 +1,123 @@
+const luParser = require('./luParser');
+const helpers = require('./helpers');
+  
+class SectionOperator {
+
+    /**
+     * @param {LUResource} luresource 
+     */
+    constructor(luresource) {
+        this.Luresource = luresource;
+    }
+
+    // TODO
+    // After CRUD, section Ids will be Scrambled and regenerated.
+    addSection(sectionContent) {
+        sectionContent = helpers.sanitizeNewLines(sectionContent);
+        this.checkSectionContent(sectionContent);
+        var newContent = `${this.Luresource.Content}\r\n${sectionContent}`;
+
+        const result = luParser.parse(newContent);
+        return result;
+    }
+
+    updateSection(id, sectionContent) {
+        sectionContent = helpers.sanitizeNewLines(sectionContent);
+        this.checkSectionContent(sectionContent);
+
+        const section = this.Luresource.Sections.find(u => u.Id === id);
+        if (!section) {
+        return this;
+        }
+
+        var startLine = section.ParseTree.start.line - 1;
+        var stopLine = section.ParseTree.stop.line - 1;
+
+        var newContent = this.replaceRangeContent(this.Luresource.Content, startLine, stopLine, sectionContent);
+
+        return luParser.parse(newContent);
+    }
+
+    deleteSection(id) {
+        const section = this.Luresource.Sections.find(u => u.Id === id);
+        if (!section) {
+        return this;
+        }
+
+        var startLine = section.ParseTree.start.line - 1;
+        var stopLine = section.ParseTree.stop.line - 1;
+
+        var newContent = this.replaceRangeContent(this.Luresource.Content, startLine, stopLine, undefined);
+
+        return luParser.parse(newContent);
+    }
+
+    checkSectionContent(sectionContent) {
+        let findSection = false;
+        const originList = sectionContent.split('\n');
+
+        for (let line of originList) {
+        if (this.isIntentSection(line) 
+            || this.isEntitySection(line) 
+            || this.isImportSection(line)
+            || this.isModelInfoSection(line)
+            || this.isQnaSection(line)) {
+            if (!findSection) {
+                findSection = true;
+                continue;
+            } else {
+                throw new Error("Please operate one section at a time.");
+            }
+            }
+        }
+
+    }
+
+    replaceRangeContent(originString, startLine, stopLine, replaceString) {
+
+        if (!originString) {
+        throw new Error('replace content with error parameters.');
+        }
+
+        const originList = originString.split('\n');
+        let destList = [];
+        if (isNaN(startLine) || isNaN(stopLine) || startLine < 0 || startLine > stopLine || originList.Length <= stopLine) {
+            throw new Error("index out of range.");
+        }
+
+        destList.push(...originList.slice(0, startLine));
+
+        if (replaceString)
+        {
+            destList.push(replaceString);
+        }
+
+        destList.push(...originList.slice(stopLine + 1));
+
+        return destList.join('\n');
+    }
+
+    isIntentSection(line) {
+        return line && line.trim().startsWith('#') && !line.trim().startsWith('##');
+    }
+
+    isEntitySection(line) {
+        return line && line.trim().startsWith('$');
+    }
+
+    isImportSection(line) {
+        const importPattern = /^\[[^\[]+\]\([^|]+\)$/gi;
+        return line && importPattern.test(line.trim());
+    }
+
+    isModelInfoSection(line) {
+        return line && line.trim().startsWith('> !#');
+    }
+
+    isQnaSection(line) {
+        const qnaPattern = /^#+ +\?/gi;
+        return line && qnaPattern.test(line.trim());
+    }
+}
+
+module.exports = SectionOperator;
