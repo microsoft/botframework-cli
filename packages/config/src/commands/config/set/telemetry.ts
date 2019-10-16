7aa1795aa1878443ce8e6d6b7626fc3e5a92aa52
Emilio Munoz <emmunozp@microsoft.com>
2019-10-16 02:22:56 -0700

    Adding flag to enable telemetry

--- packages/config/src/commands/config/set/telemetry.ts
+++ packages/config/src/commands/config/set/telemetry.ts
@@ -6,13 +6,17 @@ export default class ConfigSetTelemetry extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
-    disable: flags.boolean({char: 'd', description: 'Disable tlemetry', default: false})
+    disable: flags.boolean({char: 'd', description: 'Disable tlemetry'}),
+    enable: flags.boolean({char: 'e', description: 'Enable tlemetry'})
   }
 
   async run() {
     const {flags} = this.parse(ConfigSetTelemetry)
     let userConfig: Config = await getConfigFile(this.config.configDir)
-    userConfig.telemetry = !flags.disable
+    if (!flags.enable && !flags.disable) {
+      this._help()
+    }
+    userConfig.telemetry = flags.enable ? true : false
     await writeConfigFile(this.config.configDir, userConfig)
     this.log('Telemetry set to ' + !flags.disable)
   }
