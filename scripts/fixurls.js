93e667e36d28139e1f069397009e0206a1e6b78b
Emilio Munoz <emmunozp@microsoft.com>
2019-10-22 16:05:32 -0700

    Cleaning code

--- scripts/fixurls.js
+++ scripts/fixurls.js
@@ -1,24 +1,18 @@
 const path = require('path')
 const fs = require('fs-extra')
 
- const updateUrls = async () => {
-  try {
-    let plugins = ['config', 'chatdown', 'dialog', 'lu', 'qnamaker', 'cli']
-    for (let i = 0; i < plugins.length; i++) {
-        await compareCleanUrls(plugins[i])
-    }
-  /* tslint:disable:no-unused */
-  } catch (err) {
-      // swallow the exception; we don't want to crash the app
-      // on a failed attempt to set telemetry
+const updateUrls = async () => {
+  let plugins = ['config', 'chatdown', 'dialog', 'lu', 'qnamaker', 'cli']
+  for (let i = 0; i < plugins.length; i++) {
+      await compareCleanUrls(plugins[i])
   }
 }
 
 const compareCleanUrls = async function(plugin) {
-    let readmePath = path.join(__dirname, `./../packages/${plugin}/README.md`)
-    let fileContent = await fs.readFile(readmePath)
-    fileContent = fileContent.toString().replace(/\/blob\/v1\.0\.0/g, "")
-    await fs.writeFile(readmePath, fileContent)
+  let readmePath = path.join(__dirname, `./../packages/${plugin}/README.md`)
+  let fileContent = await fs.readFile(readmePath)
+  fileContent = fileContent.toString().replace(/\/blob\/v1\.0\.0/g, "")
+  await fs.writeFile(readmePath, fileContent)
 }
 
 const run = async () => {
