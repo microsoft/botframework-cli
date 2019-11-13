7e1aa5d2222f52b729831061be029ec8d9e9cfbd
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-11-13 15:50:33 -0800

    Adding LUIS app delete command

---
+++ packages/luis/src/commands/luis/application/delete.ts
@@ -0,0 +1,47 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {CLIError, Command, flags} from '@microsoft/bf-cli-command'
+
+const utils = require('../../../utils/index')
+
+export default class LuisApplicationDelete extends Command {
+  static description = 'Deletes a LUIS application'
+
+  static examples = [`
+    $ bf luis:application:delete --appId {APP_ID} --endpoint {ENDPOINT} --subscriptionKey {SUBSCRIPTION_KEY}
+  `]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    appId: flags.string({description: 'LUIS application Id'}),
+    endpoint: flags.string({description: 'LUIS endpoint hostname'}),
+    subscriptionKey: flags.string({description: 'LUIS cognitive services subscription key (aka Ocp-Apim-Subscription-Key)'}),
+  }
+
+  async run() {
+    const {flags} = this.parse(LuisApplicationDelete)
+    const flagLabels = Object.keys(LuisApplicationDelete.flags)
+    const configDir = this.config.configDir
+
+    const {
+      appId,
+      endpoint,
+      subscriptionKey,
+    } = await utils.processInputs(flags, flagLabels, configDir)
+
+    const requiredProps = {appId, endpoint, subscriptionKey}
+    utils.validateRequiredProps(requiredProps)
+
+    const client = utils.getLUISClient(subscriptionKey, endpoint)
+
+    try {
+      await client.apps.deleteMethod(appId)
+      this.log('App successfully deleted.')
+    } catch (err) {
+      throw new CLIError(`Failed to delete app: ${err}`)
+    }
+  }
+}
