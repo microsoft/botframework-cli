5d2aeb0b99d1e096a30855e1e07ee9ddbcb8cbe0
Chris Anderson <chrande@microsoft.com>
2019-11-13 16:26:20 -0800

    wip: added rush to command and chatdown

--- packages/config/src/commands/config/set/qnamaker.ts
+++ packages/config/src/commands/config/set/qnamaker.ts
@@ -9,7 +9,7 @@ import {getConfigFile, writeConfigFile, Config} from '../../../utils/configfileh
 export default class ConfigSetQnamaker extends Command {
   static description = 'Set the QnAMaker config data'
 
-  static flags = {
+  static flags: any = {
     kbId: flags.string({description: 'QnAMaker kbId to be set'}),
     subscriptionKey: flags.string({description: 'QnAMaker subscriptionkey to be set'}),
     hostname: flags.string({description: 'QnAMaker hostname to be set'}),
