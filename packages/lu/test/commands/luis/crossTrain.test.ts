79a4094c312275bd56ee0756c0ed6244fab4a296
Fei Chen <feich@microsoft.com>
2019-11-19 20:53:44 +0800

    support nestedIntentSection

--- packages/lu/test/commands/luis/crossTrain.test.ts
+++ packages/lu/test/commands/luis/crossTrain.test.ts
@@ -37,5 +37,16 @@ describe('luis:convert interuption intent among lu files', () => {
       expect(await compareLuFiles('./../../../interuptionGen/dia1.lu', './../../fixtures/verified/interuption2/dia1.lu')).to.be.true;
       expect(await compareLuFiles('./../../../interuptionGen/dia3.lu', './../../fixtures/verified/interuption2/dia3.lu')).to.be.true;
     })
+
+    test
+    .stdout()
+    .command(['luis:cross-train', '--in', `${path.join(__dirname, './../../fixtures/testcases/interuption3')}`, '--root', `${path.join(__dirname, './../../fixtures/testcases/interuption3/main/main.lu')}`, '--out', 'interuptionGen', '--intentname', '_Interuption', '--recurse'])
+    .it('luis:convert interuption intents when nestedIntentSection is enabled', async () => {
+      expect(await compareLuFiles('./../../../interuptionGen/main.lu', './../../fixtures/verified/interuption3/main.lu')).to.be.true;
+      expect(await compareLuFiles('./../../../interuptionGen/dia1.lu', './../../fixtures/verified/interuption3/dia1.lu')).to.be.true;
+      expect(await compareLuFiles('./../../../interuptionGen/dia2.lu', './../../fixtures/verified/interuption3/dia2.lu')).to.be.true;
+      expect(await compareLuFiles('./../../../interuptionGen/dia3.lu', './../../fixtures/verified/interuption3/dia3.lu')).to.be.true;
+      expect(await compareLuFiles('./../../../interuptionGen/dia4.lu', './../../fixtures/verified/interuption3/dia4.lu')).to.be.true;
+    })
 })
 
