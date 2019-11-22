7d5e55365170262279425bd86ad771c82dea6cbf
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-11-22 13:09:29 -0800

    Add luis:train:show cmd

---
+++ packages/luis/src/commands/luis/train/show.ts
@@ -0,0 +1,48 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {CLIError, Command, flags} from '@microsoft/bf-cli-command'
+
+const utils = require('../../../utils/index')
+
+export default class LuisTrainShow extends Command {
+  static description = 'Shows training status'
+
+  static examples = [`
+    $ bf luis:train:show --appId {APPLICATION_ID} --versionId {VERSION_ID} --endpoint {ENDPOINT} --subscriptionKey {SUBSCRIPTION_KEY}
+  `]
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    endpoint: flags.string({description: 'LUIS endpoint hostname'}),
+    subscriptionKey: flags.string({description: 'LUIS cognitive services subscription key (mandatory, default: config:LUIS:subscriptionKey)'}),
+    appId: flags.string({description: 'LUIS application Id (mandatory, defaults to config:LUIS:appId)'}),
+    versionId: flags.string({description: 'Version to show training status (mandatory, defaults to config:LUIS:versionId)'}),
+  }
+
+  async run() {
+    const {flags} = this.parse(LuisTrainShow)
+    const flagLabels = Object.keys(LuisTrainShow.flags)
+    const configDir = this.config.configDir
+
+    let {endpoint, subscriptionKey, appId, versionId} = await utils.processInputs(flags, flagLabels, configDir)
+
+    const requiredProps = {endpoint, subscriptionKey, appId, versionId}
+    utils.validateRequiredProps(requiredProps)
+
+    const client = utils.getLUISClient(subscriptionKey, endpoint)
+
+    try {
+      const trainingStatus = await client.train.getStatus(appId, versionId)
+      if (trainingStatus) {
+        await utils.writeToConsole(trainingStatus)
+        this.log('\nTraining status successfully output to console')
+      }
+    } catch (err) {
+      throw new CLIError(`Failed to retrieve training status: ${err}`)
+    }
+  }
+
+}
