491a112cc21c3b562754ed70725d3187f648b290
Jonathan Spruance (Insight Global) <v-jonspr@microsoft.com>
2019-12-20 14:43:53 -0800

    Adding custom cmd for posttest, fix lint issues

--- packages/dialog/src/commands/dialog/generate.ts
+++ packages/dialog/src/commands/dialog/generate.ts
@@ -8,6 +8,25 @@ import * as gen from '../../library/dialogGenerator'
 import * as ppath from 'path'
 
 export default class GenerateDialog extends Command {
+    static description = '[PREVIEW] Generate localized .lu, .lg, .qna and .dialog assets to define a bot based on a schema using templates.'
+
+    static examples = [`
+      $ bf dialog:generate sandwich.schema --output c:/tmp
+    `]
+
+    static args = [
+        { name: 'schema', required: true, description: 'JSON Schema .schema file used to drive generation.' }
+    ]
+
+    static flags: flags.Input<any> = {
+        force: flags.boolean({ char: 'f', description: 'Force overwriting generated files.' }),
+        help: flags.help({ char: 'h' }),
+        locale: flags.string({ char: 'l', description: 'Locales to generate. [default: en-us]', multiple: true }),
+        output: flags.string({ char: 'o', description: 'Output path for where to put generated .lu, .lg, .qna and .dialog files. [default: .]', default: '.', required: false }),
+        schema: flags.string({ char: 's', description: 'Path to your app.schema file.', required: false }),
+        templates: flags.string({ char: 't', description: 'Directory with templates to use for generating assets.', multiple: true }),
+        verbose: flags.boolean({ description: 'Output verbose logging of files as they are processed', default: false }),
+    }
 
     async run() {
         const { args, flags } = this.parse(GenerateDialog)
@@ -37,25 +56,5 @@ export default class GenerateDialog extends Command {
 
     progress(msg: string): void {
         this.error(msg)
-    }
-
-    static description = '[PREVIEW] Generate localized .lu, .lg, .qna and .dialog assets to define a bot based on a schema using templates.'
-
-    static examples = [`
-      $ bf dialog:generate sandwich.schema --output c:/tmp
-    `]
-
-    static args = [
-        { name: 'schema', required: true, description: 'JSON Schema .schema file used to drive generation.' }
-    ]
-
-    static flags: flags.Input<any> = {
-        force: flags.boolean({ char: 'f', description: 'Force overwriting generated files.' }),
-        help: flags.help({ char: 'h' }),
-        locale: flags.string({ char: 'l', description: 'Locales to generate. [default: en-us]', multiple: true }),
-        output: flags.string({ char: 'o', description: 'Output path for where to put generated .lu, .lg, .qna and .dialog files. [default: .]', default: '.', required: false }),
-        schema: flags.string({ char: 's', description: 'Path to your app.schema file.', required: false }),
-        templates: flags.string({ char: 't', description: 'Directory with templates to use for generating assets.', multiple: true }),
-        verbose: flags.boolean({ description: 'Output verbose logging of files as they are processed', default: false }),
     }
 }
