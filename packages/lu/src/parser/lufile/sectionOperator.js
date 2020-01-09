c2b36f52b6f1741db406081268a4d1725ca244db
Fei Chen <feich@microsoft.com>
2020-01-09 14:44:39 +0800

    fix isuue

--- packages/lu/src/parser/lufile/sectionOperator.js
+++ packages/lu/src/parser/lufile/sectionOperator.js
@@ -1,76 +1,75 @@
 const luParser = require('./luParser');
 const helpers = require('./../utils/helpers');
-  
+
 class SectionOperator {
 
-    /**
-     * @param {LUResource} luresource 
-     */
-    constructor(luresource) {
-        this.Luresource = luresource;
+  /**
+   * @param {LUResource} luresource 
+   */
+  constructor(luresource) {
+    this.Luresource = luresource;
+  }
+
+  // After CRUD, section Ids will keep same unless you change section name.
+  addSection(sectionContent) {
+    sectionContent = helpers.sanitizeNewLines(sectionContent);
+    var newContent = `${this.Luresource.Content}\r\n${sectionContent}`;
+
+    const result = luParser.parse(newContent);
+    return result;
+  }
+
+  updateSection(id, sectionContent) {
+    sectionContent = helpers.sanitizeNewLines(sectionContent);
+    const section = this.Luresource.Sections.find(u => u.Id === id);
+    if (!section) {
+      return this.Luresource;
     }
 
-    // After CRUD, section Ids will keep same unless you change section name.
-    addSection(sectionContent) {
-        sectionContent = helpers.sanitizeNewLines(sectionContent);
-        var newContent = `${this.Luresource.Content}\r\n${sectionContent}`;
+    var startLine = section.ParseTree.start.line - 1;
+    var stopLine = section.ParseTree.stop.line - 1;
 
-        const result = luParser.parse(newContent);
-        return result;
+    var newContent = this.replaceRangeContent(this.Luresource.Content, startLine, stopLine, sectionContent);
+
+    return luParser.parse(newContent);
+  }
+
+  deleteSection(id) {
+    const section = this.Luresource.Sections.find(u => u.Id === id);
+    if (!section) {
+      return this;
     }
 
-    updateSection(id, sectionContent) {
-        sectionContent = helpers.sanitizeNewLines(sectionContent);
-        const section = this.Luresource.Sections.find(u => u.Id === id);
-        if (!section) {
-        return this;
-        }
+    var startLine = section.ParseTree.start.line - 1;
+    var stopLine = section.ParseTree.stop.line - 1;
 
-        var startLine = section.ParseTree.start.line - 1;
-        var stopLine = section.ParseTree.stop.line - 1;
+    var newContent = this.replaceRangeContent(this.Luresource.Content, startLine, stopLine, undefined);
 
-        var newContent = this.replaceRangeContent(this.Luresource.Content, startLine, stopLine, sectionContent);
+    return luParser.parse(newContent);
+  }
 
-        return luParser.parse(newContent);
+  replaceRangeContent(originString, startLine, stopLine, replaceString) {
+
+    if (!originString) {
+      throw new Error('replace content with error parameters.');
     }
 
-    deleteSection(id) {
-        const section = this.Luresource.Sections.find(u => u.Id === id);
-        if (!section) {
-            return this;
-        }
-
-        var startLine = section.ParseTree.start.line - 1;
-        var stopLine = section.ParseTree.stop.line - 1;
-
-        var newContent = this.replaceRangeContent(this.Luresource.Content, startLine, stopLine, undefined);
-
-        return luParser.parse(newContent);
+    const originList = originString.split(/\r?\n/);
+    let destList = [];
+    if (isNaN(startLine) || isNaN(stopLine) || startLine < 0 || startLine > stopLine || originList.Length <= stopLine) {
+      throw new Error("index out of range.");
     }
 
-    replaceRangeContent(originString, startLine, stopLine, replaceString) {
+    destList.push(...originList.slice(0, startLine));
 
-        if (!originString) {
-            throw new Error('replace content with error parameters.');
-        }
-
-        const originList = originString.split(/\r?\n/);
-        let destList = [];
-        if (isNaN(startLine) || isNaN(stopLine) || startLine < 0 || startLine > stopLine || originList.Length <= stopLine) {
-            throw new Error("index out of range.");
-        }
-
-        destList.push(...originList.slice(0, startLine));
-
-        if (replaceString)
-        {
-            destList.push(replaceString);
-        }
-
-        destList.push(...originList.slice(stopLine + 1));
-
-        return destList.join('\n');
+    if (replaceString) {
+      destList.push(replaceString);
     }
+
+    destList.push(...originList.slice(stopLine + 1));
+
+    return destList.join('\n');
+  }
 }
 
 module.exports = SectionOperator;
