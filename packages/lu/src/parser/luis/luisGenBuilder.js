7ce505aa0993a21e604e1658c2aa76f0f63c0262
Axel Suarez <axsuarez@microsoft.com>
2020-02-04 19:04:38 -0800

    initial changes to suppor 6.0.0

--- packages/lu/src/parser/luis/luisGenBuilder.js
+++ packages/lu/src/parser/luis/luisGenBuilder.js
@@ -4,23 +4,50 @@ const {CLIError} = require('@microsoft/bf-cli-command')
 
 class LuisGenBuilder {
     static build(luisApp) {
-        let result = new LuisGen()
-        try {
-            result.intents = processIntents(luisApp.intents);
-            result.entities = extractEntities(luisApp.entities);
-            result.prebuiltEntities = extractEntities(luisApp.prebuiltEntities, true);
-            result.closedLists = extractEntities(luisApp.closedLists);
-            result.regex_entities = extractEntities(luisApp.regex_entities);
-            result.patternAnyEntities = extractEntities(luisApp.patternAnyEntities);
-            result.composites = extractComposites(luisApp.composites);
-        } catch (err) {
-            throw new CLIError("Invalid LUIS JSON file content.")
+        let buildWithVersion;
+        if (luisApp.luis_schema_version < "5") {
+            buildWithVersion = buildUpToVersion4;
+        } else if (luisApp.luis_schema_version >= "6.0.0") {
+            buildWithVersion = buildVersion6;
+        } else {
+            throw new CLIError("Invalid LUIS JSON schema version.")
         }
-        return result
+
+        return buildWithVersion(luisApp);
     }
 }
 
 module.exports = LuisGenBuilder
+
+const buildUpToVersion4 = function(luisApp) {
+    let result = new LuisGen()
+    try {
+        result.intents = processIntents(luisApp.intents);
+        result.entities = extractEntities(luisApp.entities);
+        result.prebuiltEntities = extractEntities(luisApp.prebuiltEntities, true);
+        result.closedLists = extractEntities(luisApp.closedLists);
+        result.regex_entities = extractEntities(luisApp.regex_entities);
+        result.patternAnyEntities = extractEntities(luisApp.patternAnyEntities);
+        result.composites = extractComposites(luisApp.composites);
+    } catch (err) {
+        throw new CLIError("Invalid LUIS JSON file content.")
+    }
+}
+
+const buildVersion6 = function(luisApp) {
+    let result = new LuisGen()
+    try {
+        result.intents = processIntents(luisApp.intents);
+        result.entities = extractEntities(luisApp.entities);
+        result.prebuiltEntities = extractEntities(luisApp.prebuiltEntities, true);
+        result.closedLists = extractEntities(luisApp.closedLists);
+        result.regex_entities = extractEntities(luisApp.regex_entities);
+        result.patternAnyEntities = extractEntities(luisApp.patternAnyEntities);
+        result.composites = [];
+    } catch (err) {
+        throw new CLIError("Invalid LUIS JSON file content.")
+    }
+}
 
 const processIntents = function(intents) {
     const result = [];
@@ -65,6 +92,28 @@ const extractEntities = function(entities, builtIn = false) {
         }
         else {
             result.push(...aux);
+        }
+    });
+    return result;
+}
+
+const extractEntitiesV6 = function(entities) {
+    const simple_entities_result = [];
+    const composite_entities_result = [];
+
+    entities.forEach(entity => {
+        if (Array.isArray(entity.children) && entity.children.length) {
+            // is composite
+            
+        } else {
+            // is simple entity
+            if (entity.instanceOf) {
+                simple_entities_result.push(
+                    { entityName: entity.name, instanceOf: entity.instanceOf }
+                )
+            } else {
+                simple_entities_result.push(entity.name)
+            }
         }
     });
     return result;
