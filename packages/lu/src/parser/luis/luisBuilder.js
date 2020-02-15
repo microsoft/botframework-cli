38e3073d4e1a26e643298f1f5a0b1486911a869c
Emilio Munoz <emmunozp@microsoft.com>
2020-02-14 16:25:09 -0800

    Moving LU commands to Luis and QnAMaker

--- packages/lu/src/parser/luis/luisBuilder.js
+++ packages/lu/src/parser/luis/luisBuilder.js
@@ -2,6 +2,38 @@ const mergeLuFiles = require('./../lu/luMerger').Build
 const Luis = require('./luis')
 
 class LuisBuilder {
+
+    /**
+     * Builds a Luis instance from a Lu list.
+     * @param {JSON} luisJson Luis json
+     * @returns {Luis} new Luis instance
+     * @throws {exception} Throws on errors. exception object includes errCode and text. 
+     */
+    static async buildFromLuisJson(luisJson) {
+        return new Luis(luisJson)
+    }
+
+    /**
+     * Builds a Luis instance from a Lu list.
+     * @param {LU} luObject LU instance
+     * @returns {Luis} new Luis instance
+     * @throws {exception} Throws on errors. exception object includes errCode and text. 
+     */
+    static async buildFromLU(luObject) {
+        return this.buildFromLUList([luObject])
+    }
+
+    /**
+     * Builds a Luis instance from a Lu list.
+     * @param {Array<LU>} luArray Array of LU files to be merge
+     * @param {function} luSearchFn function to retrieve the lu files found in the references
+     * @returns {Luis} new Luis instance
+     * @throws {exception} Throws on errors. exception object includes errCode and text. 
+     */
+    static async buildFromLUList(luArray, luSearchFn) {
+        return this.build(luArray, false, '', luSearchFn)
+    }
+
     /**
      * Builds a Luis instance from a Lu list.
      * @param {Array<Lu>} luObjArray Array of LU files to be merge
@@ -26,4 +58,3 @@ class LuisBuilder {
 }
 
 module.exports = LuisBuilder
-
---
+++ packages/luis/test/utils/filehelper.test.js
@@ -0,0 +1,38 @@
+const utils = require('@microsoft/bf-cli-command').utils
+const expect = require('chai').expect;   
+const fileHelper = require('./../../src/utils/filehelper')
+const luObject = require('./../../node_modules/@microsoft/bf-lu/lib/parser/lu/lu')
+const path = require('path')
+
+describe('utils/filehelper test', () => {
+    it('File helper correctly  builds a luObject list from a file', async function(){
+        try{
+            let expected = []
+            let pathToFile = path.resolve(path.join(__dirname, './../fixtures/file.lu'))
+            let content = await utils.readTextFile(pathToFile)
+            expected.push(new luObject(content, pathToFile))
+            let luObjArray = await fileHelper.getLuObjects('', pathToFile)
+            expect(luObjArray).to.deep.equal(expected)
+        }catch(err){
+            console.log(err)
+        }
+    })
+
+    it('File helper correctly  builds a luObject list from stdin', async function(){
+        try{
+            let content = `> Definition for greeting intent
+            # Greeting
+            - Hi
+            - Hello
+            > users might say these
+            - Good morning 
+            - Good evening`
+            let luObjArray = await fileHelper.getLuObjects(content, '')
+            let expected = []
+            expected.push(new luObject(content, 'stdin'))
+            expect(luObjArray).to.deep.equal(expected)
+        }catch(err){
+            console.log(err)
+        }
+    })
+})
