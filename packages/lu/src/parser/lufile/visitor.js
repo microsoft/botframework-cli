89fa7a2a7e3d88407421edd41708eeb219e17073
Vishwac Sena Kannan <vkannan@microsoft.com>
2020-02-18 19:13:00 -0800

    initial rewrite

--- packages/lu/src/parser/lufile/visitor.js
+++ packages/lu/src/parser/lufile/visitor.js
@@ -61,44 +61,87 @@ class Visitor {
     static extractEntityFromUtterence(exp) {
         let entities = [];
         let errorMsgs = [];
+        let entityBuffer = [];
+        let entityNameCapture = false;
+        let entityValueCapture = false;
+        let entityRoleCapture = false;
+        //exp = exp.substring(1, exp.length - 1).trim();
+        if (exp.indexOf('=') !== -1) {
+            exp.split('').forEach(char => {
+                switch(char) 
+                {
+                    case '{':
+                        // add a new entity at index 0
+                        entityBuffer = [{entityName : '', role : '', entityValue : ''}].concat(entityBuffer)
+                        entityNameCapture = true;
+                        entityRoleCapture = false;
+                        entityValueCapture = false;
+                        break;
+                    case '}':
+                        entities.push(entityBuffer.splice(0,1));
+                        entityValueCapture = false;
+                        entityRoleCapture = false;
+                        entityNameCapture = false;
+                        break;
 
-        exp = exp.substring(1, exp.length - 1).trim();
-        let equalIndex = exp.indexOf('=');
-        if (equalIndex !== -1) {
+                    case '=':
+                        entityNameCapture = false;
+                        entityValueCapture = true;
+                        entityRoleCapture = false;
+                        break;
+
+                    case ':':
+                        entityNameCapture = false;
+                        entityValueCapture = false;
+                        entityRoleCapture = true;
+                        break;
+
+                    default :
+                        if (entityNameCapture) {
+                            entityBuffer[0].entityName += char;
+                        } else if (entityValueCapture) {
+                            entityBuffer[0].entityValue += char;
+                        } else if (entityRoleCapture) {
+                            entityBuffer[0].role += char;
+                        }
+                        break;
+
+                }
+            })
             // entity with labelled value
-            let entityName = exp.substring(0, equalIndex).trim();
-            let entityValue = exp.substring(equalIndex + 1).trim();
+            // let entityName = exp.substring(0, equalIndex).trim();
+            // let entityValue = exp.substring(equalIndex + 1).trim();
 
-            let updatedEntityValue = entityValue;
-            let compositeEntityRightIndex = updatedEntityValue.indexOf('}');
-            let compositeEntityLeftIndex = updatedEntityValue.substring(0, compositeEntityRightIndex).lastIndexOf('{');
-            while (compositeEntityLeftIndex > -1 && compositeEntityRightIndex > compositeEntityLeftIndex) {
-                // composite entities
-                let compositeEntityDefinition = updatedEntityValue.substring(compositeEntityLeftIndex + 1, compositeEntityRightIndex).trim();
-                let compositeEntityEqualIndex = compositeEntityDefinition.indexOf('=');
-                if (compositeEntityEqualIndex !== -1) {
-                    let compositeEntityName = compositeEntityDefinition.substring(0, compositeEntityEqualIndex).trim();
-                    let compositeEntityValue = compositeEntityDefinition.substring(compositeEntityEqualIndex + 1).trim();
-                    entities.push({ entityName: compositeEntityName, entityValue: compositeEntityValue });
-                    updatedEntityValue = updatedEntityValue.substring(0, compositeEntityLeftIndex) + compositeEntityValue + updatedEntityValue.substring(compositeEntityRightIndex + 1);
-                    compositeEntityRightIndex = updatedEntityValue.indexOf('}');
-                    compositeEntityLeftIndex = updatedEntityValue.substring(0, compositeEntityRightIndex).lastIndexOf('{');
-                } else {
-                    errorMsgs.push(`Composite entity "${entityName}" includes pattern.any entity "${compositeEntityDefinition}".\r\n\tComposites cannot include pattern.any entity as a child.`)
-                    break;
-                }
-            }
+            // let updatedEntityValue = entityValue;
+            // let compositeEntityRightIndex = updatedEntityValue.indexOf('}');
+            // let compositeEntityLeftIndex = updatedEntityValue.substring(0, compositeEntityRightIndex).lastIndexOf('{');
+            // while (compositeEntityLeftIndex > -1 && compositeEntityRightIndex > compositeEntityLeftIndex) {
+            //     // composite entities
+            //     let compositeEntityDefinition = updatedEntityValue.substring(compositeEntityLeftIndex + 1, compositeEntityRightIndex).trim();
+            //     let compositeEntityEqualIndex = compositeEntityDefinition.indexOf('=');
+            //     if (compositeEntityEqualIndex !== -1) {
+            //         let compositeEntityName = compositeEntityDefinition.substring(0, compositeEntityEqualIndex).trim();
+            //         let compositeEntityValue = compositeEntityDefinition.substring(compositeEntityEqualIndex + 1).trim();
+            //         entities.push({ entityName: compositeEntityName, entityValue: compositeEntityValue });
+            //         updatedEntityValue = updatedEntityValue.substring(0, compositeEntityLeftIndex) + compositeEntityValue + updatedEntityValue.substring(compositeEntityRightIndex + 1);
+            //         compositeEntityRightIndex = updatedEntityValue.indexOf('}');
+            //         compositeEntityLeftIndex = updatedEntityValue.substring(0, compositeEntityRightIndex).lastIndexOf('{');
+            //     } else {
+            //         errorMsgs.push(`Composite entity "${entityName}" includes pattern.any entity "${compositeEntityDefinition}".\r\n\tComposites cannot include pattern.any entity as a child.`)
+            //         break;
+            //     }
+            // }
 
-            entities.push({ entityName: entityName, entityValue: updatedEntityValue });
-            entities.forEach(entity => {
-                let colonIndex = entity.entityName.indexOf(':');
-                if (colonIndex !== -1) {
-                    let entityName = entity.entityName.substring(0, colonIndex).trim();
-                    let roleName = entity.entityName.substring(colonIndex + 1).trim();
-                    entity.entityName = entityName;
-                    entity.role = roleName;
-                }
-            });
+            // entities.push({ entityName: entityName, entityValue: updatedEntityValue });
+            // entities.forEach(entity => {
+            //     let colonIndex = entity.entityName.indexOf(':');
+            //     if (colonIndex !== -1) {
+            //         let entityName = entity.entityName.substring(0, colonIndex).trim();
+            //         let roleName = entity.entityName.substring(colonIndex + 1).trim();
+            //         entity.entityName = entityName;
+            //         entity.role = roleName;
+            //     }
+            // });
         } else {
             // pattern.any entity
             let colonIndex = exp.indexOf(':');
