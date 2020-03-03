0f9742f1c6da9aab045c76ff068983d85da110fb
Vishwac Sena Kannan <vishwacsenakannan@Vishwacs-MBP.guest.corp.microsoft.com>
2020-03-03 11:59:46 -0800

    Adding tests and API updates

--- packages/lu/test/parser/luis/luisBuilder.test.js
+++ packages/lu/test/parser/luis/luisBuilder.test.js
@@ -4,13 +4,28 @@ var chai = require('chai');
 var assert = chai.assert;
 
 describe('LUISBuilder', function() {
+    it('Build app from LU content', async() => {
+        let luContent1 = `# Greeting
+- hi`;
+
+        const luisObject = await LUISBuilder.fromLUAsync(luContent1)
+        assert.equal(luisObject.intents.length, 1)
+    })
+    it('Validate test', async() => {
+        let luContent = `# Greeting
+        - hi`;
+
+const luisObject = await LUISBuilder.fromLUAsync(luContent)
+luisObject.intents[0].name = "testIntent123456789012345678901234567890123"
+assert.isTrue(luisObject.validate())
+    })
     it('Build luis app from LU content', async () => {
         let luFile = `
         @ ml test
         # test
         - this is a {@test = one}
         `;
-        const luisObject = await LUISBuilder.fromContent(luFile)
+        const luisObject = await LUISBuilder.fromContentAsync(luFile)
         assert.equal(luisObject.entities.length, 1);
         assert.equal(luisObject.entities[0].name, 'test');
         assert.equal(luisObject.utterances.length, 1);
@@ -25,8 +40,7 @@ describe('LUISBuilder', function() {
         - this is a {@test = one}
         `;
         const luObject = new LU(luFile, '')
-        const luisObject = await LUISBuilder.fromLU([luObject])
-
+        const luisObject = await LUISBuilder.fromLUAsync([luObject])
         assert.equal(luisObject.entities.length, 1);
         assert.equal(luisObject.entities[0].name, 'test');
         assert.equal(luisObject.utterances.length, 1);
@@ -39,7 +53,7 @@ describe('LUISBuilder', function() {
         let testLU2 = `$userName:simple role=lastName`;
         const luObject1 = new LU(testLU1)
         const luObject2 = new LU(testLU2)
-        const luisObject = await LUISBuilder.fromLU([luObject1, luObject2])
+        const luisObject = await LUISBuilder.fromLUAsync([luObject1, luObject2])
         assert.equal(luisObject.entities.length, 1);
         assert.equal(luisObject.entities[0].roles.length, 2);
         assert.deepEqual(luisObject.entities[0].roles, ['firstName', 'lastName']);
