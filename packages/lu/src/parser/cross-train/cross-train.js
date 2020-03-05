13300c59a30cdc07f54a6a9d2a9a349bfa75b872
Fei Chen <feich@microsoft.com>
2020-03-05 14:38:24 +0800

    fix config id issue

--- packages/lu/src/parser/cross-train/cross-train.js
+++ packages/lu/src/parser/cross-train/cross-train.js
@@ -23,7 +23,7 @@ module.exports = {
     // Get all related file content.
     const luContents = await file.getFilesContent(input, fileExtEnum.LUFile)
     const qnaContents = await file.getFilesContent(input, fileExtEnum.QnAFile)
-    const configContent = config && !fs.existsSync(config) ? config : await file.getConfigContent(config || input)
+    const configContent = config && !fs.existsSync(config) ? {id: path.join(input, 'config.json'), content: config} : await file.getConfigContent(config || input)
 
     const configObject = file.getConfigObject(configContent, intentName)
 
