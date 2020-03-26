8d656f134b84654b994ce9a970e383c929206afb
Cecilia Avila <cecilia.avila@southworks.com>
2020-03-26 15:40:33 -0300

    Add luis application:create command

--- packages/bfCliTask/src/index.ts
+++ packages/bfCliTask/src/index.ts
@@ -7,8 +7,10 @@ import taskLibrary = require('azure-pipelines-task-lib/task');
 import { execSync } from "child_process";
 import { SubscriptionHelper } from './subscriptionHelper';
 import { InputValues } from './inputValues';
+import { readFileSync } from 'fs';
 
 const input = new InputValues();
+const rootPath = taskLibrary.getVariable('System.DefaultWorkingDirectory');
 
 const azureLogin = (helper: SubscriptionHelper): void => {
     const userName = helper.getServicePrincipalClientId();
@@ -22,10 +24,47 @@ const azureLogin = (helper: SubscriptionHelper): void => {
     console.log('Successful login');
 }
 
+const checkInstalledTool = (): boolean => {
+    const output: string = `${ rootPath }/Output.txt`;
+    const command = `bf -v > ${ output }`;
+    let buffer: Buffer;
+
+    try {
+        console.log('Checking bf cli installation...');
+
+        execSync(command);
+        buffer = readFileSync(output);
+        const version = buffer.toString('utf-8').trim();
+
+        if (version.substr(0,27) === '@microsoft/botframework-cli') {
+            console.log('bf cli tool version: ' + version); 
+            return true;
+        } 
+        else {
+            console.log('bf cli tool not installed'); 
+            return false;
+        }
+    } catch {
+        console.log('bf cli tool not installed'); 
+        return false;
+    }
+}
+
+const installBfCliTool = (): void => {
+    const command = `npm i -g @microsoft/botframework-cli`;  
+    
+    console.log('Installing bf cli tool...');
+    
+    execSync(command);
+    
+    console.log('bf cli installed successfully');      
+}
+
 const createLuisApplication = (): void => {
     console.log('Creating LUIS Application...');
 
-    let command = `az group create --name "${ input.applicationName }" --subscriptionKey "XXXXXXXXX" --versionId "1.0"`;
+    let command = `bf luis:application:create --name "${ input.luisApplicationName }" --endpoint "${ input.luisEndpoint }" --subscriptionKey "${ input.luisSubscriptionKey }" `;
+    command += `--culture "${ input.luisCulture }" --description "${ input.luisAppDescription }" --versionId "${ input.luisVersionId }"`
     
     execSync(command);
     console.log('LUIS Application successfully created');      
@@ -37,7 +76,19 @@ const run = (): void => {
 
     azureLogin(helper);
 
-    createLuisApplication();
+    if (!checkInstalledTool()) {
+        installBfCliTool();
+    }
+
+    if (input.luisCommand) {
+        switch (input.luisSubCommand) {
+            case 'ApplicationCreate':
+                createLuisApplication();
+                break;
+            default:
+                console.log('No LUIS Command was selected.');
+        }
+    }
 }
 
 run();
--- packages/bfCliTask/src/inputValues.ts
+++ packages/bfCliTask/src/inputValues.ts
@@ -3,15 +3,28 @@
  * Licensed under the MIT License.
  */
 
-import { getInput } from "azure-pipelines-task-lib";
+import { getInput, getBoolInput } from "azure-pipelines-task-lib";
 
 export class InputValues {
 
-    public bfCommand: string;
-    public applicationName: string;
+    public luisCommand: boolean;
+    public luisSubCommand: string;
+    public luisApplicationName: string;
+    public luisEndpoint: string;
+    public luisSubscriptionKey: string;
+    public luisCulture: string;
+    public luisVersionId: string;
+    public luisAppDescription: string;
 
     constructor() {
-        this.bfCommand = getInput('bfCommand', true) as string;
-        this.applicationName = getInput('applicationName', true) as string;
+        // LUIS inputs
+        this.luisCommand = getBoolInput('luisCommand', false);
+        this.luisSubCommand = getInput('luisSubCommand', true) as string;
+        this.luisApplicationName = getInput('luisApplicationName', true) as string;
+        this.luisEndpoint = getInput('luisEndpoint', true) as string;
+        this.luisSubscriptionKey = getInput('luisSubscriptionKey', true) as string;
+        this.luisCulture = getInput('luisCulture', true) as string;
+        this.luisVersionId = getInput('luisVersionId', true) as string;
+        this.luisAppDescription = getInput('luisAppDescription', true) as string;
     } 
 }
