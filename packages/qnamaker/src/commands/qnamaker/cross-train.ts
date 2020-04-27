68ae4f1262f5dafb368bf54320c21fcb03766522
Fei Chen <feich@microsoft.com>
2020-04-27 17:53:41 +0800

    add cross train in qna maker cli

---
+++ packages/qnamaker/src/commands/qnamaker/cross-train.ts
@@ -0,0 +1,50 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {CLIError, Command, flags} from '@microsoft/bf-cli-command'
+const crossTrain = require('@microsoft/bf-lu/lib/parser/cross-train/cross-train')
+const exception = require('@microsoft/bf-lu/lib/parser/utils/exception')
+const path = require('path')
+
+export default class QnamakerCrossTrain extends Command {
+  static description = 'Lu and Qna cross train tool'
+
+  static flags: flags.Input<any> = {
+    help: flags.help({char: 'h', description: 'luis:cross-train help'}),
+    in: flags.string({char: 'i', description: 'source lu and qna files folder'}),
+    out: flags.string({char: 'o', description: 'output folder name. If not specified, the cross trained files will be wrote to cross-trained folder under folder of current command'}),
+    config: flags.string({description: 'path to config file of mapping rules which is relative to folder specified by --in. If not specified, it will read default config.json from the folder specified by --in'}),
+    intentName: flags.string({description: 'Interruption intent name', default: '_Interruption'}),
+    rootDialog: flags.string({description: 'rootDialog file path which is relative to folder specified by --in. If --config not specified, cross-trian will automatically construct the config from file system based on root dialog file'})
+  }
+
+  async run() {
+    try {
+      const {flags} = this.parse(QnamakerCrossTrain)
+
+      if (!path.isAbsolute(flags.in)) flags.in = path.resolve(flags.in)
+      if (flags.config && flags.config !== '') {
+        if (!path.isAbsolute(flags.config)) flags.config = path.join(flags.in, flags.config)
+      } else if (flags.rootDialog && flags.rootDialog !== '') {
+        if (!path.isAbsolute(flags.rootDialog)) flags.rootDialog = path.join(flags.in, flags.rootDialog)
+        flags.config = await crossTrain.generateConfig(flags.in, flags.rootDialog)
+      }
+
+      const trainedResult = await crossTrain.train(flags.in, flags.intentName, flags.config)
+
+      if (flags.out === undefined) {
+        flags.out = path.join(process.cwd(), 'cross-trained')
+      }
+
+      await crossTrain.writeFiles(trainedResult.luResult, flags.out)
+      await crossTrain.writeFiles(trainedResult.qnaResult, flags.out)
+    } catch (err) {
+      if (err instanceof exception) {
+        throw new CLIError(err.text)
+      }
+      throw err
+    }
+  }
+}
---
+++ packages/qnamaker/test/commands/qnamaker/crossTrain.test.ts
@@ -0,0 +1,80 @@
+import {expect, test} from '@oclif/test'
+const fs = require('fs-extra')
+const path = require('path')
+
+const compareLuFiles = async function (file1: string, file2: string) {
+  let result = await fs.readFile(path.join(__dirname, file1))
+  let fixtureFile = await fs.readFile(path.join(__dirname, file2))
+  result = result.toString().replace(/\r\n/g, "\n")
+  fixtureFile = fixtureFile.toString().replace(/\r\n/g, "\n")
+  return result === fixtureFile
+}
+
+describe('qnamaker:cross training tests among lu and qna contents', () => {
+  after(async function () {
+    await fs.remove(path.join(__dirname, './../../../interruptionGen'))
+  })
+
+  test
+    .stdout()
+    .command(['qnamaker:cross-train',
+      '--in', `${path.join(__dirname, './../../fixtures/testcases/interruption')}`,
+      '--intentName', '_Interruption',
+      '--config', `${path.join(__dirname, './../../fixtures/testcases/interruption/mapping_rules.json')}`,
+      '--out', './interruptionGen'])
+    .it('qnamaker:cross training can get expected result when handling multi locales and duplications', async () => {
+      expect(await compareLuFiles('./../../../interruptionGen/main.lu', './../../fixtures/verified/interruption/main.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/main.qna', './../../fixtures/verified/interruption/main.qna')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.lu', './../../fixtures/verified/interruption/dia1.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.qna', './../../fixtures/verified/interruption/dia1.qna')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia2.lu', './../../fixtures/verified/interruption/dia2.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia2.qna', './../../fixtures/verified/interruption/dia2.qna')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia3.lu', './../../fixtures/verified/interruption/dia3.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia3.qna', './../../fixtures/verified/interruption/dia3.qna')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia4.lu', './../../fixtures/verified/interruption/dia4.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/main.fr-fr.lu', './../../fixtures/verified/interruption/main.fr-fr.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/main.fr-fr.qna', './../../fixtures/verified/interruption/main.fr-fr.qna')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.fr-fr.lu', './../../fixtures/verified/interruption/dia1.fr-fr.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.fr-fr.qna', './../../fixtures/verified/interruption/dia1.fr-fr.qna')).to.be.true
+    })
+
+  test
+    .stdout()
+    .command(['qnamaker:cross-train',
+      '--in', `${path.join(__dirname, './../../fixtures/testcases/interruption2')}`,
+      '--intentName', '_Interruption',
+      '--out', './interruptionGen'])
+    .it('qnamaker:cross training can get expected result when nestedIntentSection is enabled', async () => {
+      expect(await compareLuFiles('./../../../interruptionGen/main.lu', './../../fixtures/verified/interruption2/main.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.lu', './../../fixtures/verified/interruption2/dia1.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia2.lu', './../../fixtures/verified/interruption2/dia2.lu')).to.be.true
+    })
+
+  test
+    .stdout()
+    .command(['qnamaker:cross-train',
+      '--in', `${path.join(__dirname, './../../fixtures/testcases/interruption3')}`,
+      '--intentName', '_Interruption',
+      '--out', './interruptionGen'])
+    .it('qnamaker:cross training can get expected result when multiple dialog invocations occur in same trigger', async () => {
+      expect(await compareLuFiles('./../../../interruptionGen/main.lu', './../../fixtures/verified/interruption3/main.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.lu', './../../fixtures/verified/interruption3/dia1.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia2.lu', './../../fixtures/verified/interruption3/dia2.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia3.lu', './../../fixtures/verified/interruption3/dia3.lu')).to.be.true
+    })
+
+  test
+    .stdout()
+    .command(['qnamaker:cross-train',
+      '--in', `${path.join(__dirname, './../../fixtures/testcases/interruption4')}`,
+      '--intentName', '_Interruption',
+      '--out', './interruptionGen',
+      '--rootDialog', 'main/main.dialog'])
+    .it('qnamaker:cross training can get expected result when automatically detecting config based on rootdialog and file system', async () => {
+      expect(await compareLuFiles('./../../../interruptionGen/main.lu', './../../fixtures/verified/interruption4/main.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia1.lu', './../../fixtures/verified/interruption4/dia1.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia2.lu', './../../fixtures/verified/interruption4/dia2.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia3.lu', './../../fixtures/verified/interruption4/dia3.lu')).to.be.true
+      expect(await compareLuFiles('./../../../interruptionGen/dia4.lu', './../../fixtures/verified/interruption4/dia4.lu')).to.be.true
+    })
+})
