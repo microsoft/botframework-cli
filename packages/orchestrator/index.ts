157ab45d7f6e094e375561a911d772ea98b1fe71
Tien Suwandy <tiens@microsoft.com>
2020-06-26 12:13:17 -0700

    get oc node wrapper working from build command

---
+++ packages/orchestrator/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @module oc_node
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+var addon = require('bindings')('oc_node');
--- packages/orchestrator/src/commands/orchestrator/build.ts
+++ packages/orchestrator/src/commands/orchestrator/build.ts
@@ -8,6 +8,9 @@ import {Command, CLIError, flags} from '@microsoft/bf-cli-command';
 import {Utility} from '../../utils/utility';
 import {OrchestratorHelper} from '../../utils';
 
+const oc = require('bindings')('oc_node');
+const util = require('util')
+
 export default class OrchestratorBuild extends Command {
   static description = 'describe the command here'
 
@@ -29,5 +32,79 @@ export default class OrchestratorBuild extends Command {
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
     }
+
+    this.log('Create Orchestrator..')
+    const orchestrator = new oc.Orchestrator();
+
+    this.log('Loading NLR..')
+    const load_result = await orchestrator.load('D:\\src\\TScience\\Orchestrator\\oc\\dep\\model'); // Return boolean, separate load.
+    if (load_result === false)
+    {
+      this.log('Loading NLR failed!!');
+    }
+
+    this.log('Creating labeler..');
+    let labeler = orchestrator.createLabelResolver(); 
+    
+    const example = { 
+        label: 'travel', 
+        text: 'book a flight to miami.',
+        };
+    
+    this.log('Adding example..');
+    var val = labeler.addExample(example);
+    if (val == true)
+    {
+      this.log('Added example!');
+    }
+
+    const example2 = { 
+      label: 'schedule', 
+      text: 'when is my next appointment?',
+      };
+    val = labeler.addExample(example2);
+    if (val == true)
+    {
+        this.log('Added example2!');
+    }
+    const example3 = { 
+        label: 'greeting', 
+        text: 'hello there!',
+        };
+    val = labeler.addExample(example3);
+    if (val == true)
+    {
+      this.log('Added example3!');
+    }
+
+
+    var results = labeler.score("hey");
+    this.log(util.inspect(results, true, null, true /* enable colors */));
+    var snapshot = labeler.createSnapshot();
+    this.log('Created snapshot!');
+    this.log('Going to create labeler #2');
+    let labeler2 = orchestrator.createLabelResolver(snapshot); 
+    this.log('Created Labeler #2.');
+
+
+    //
+    // Get Examples
+    //
+    console.log('Getting examples')
+    let examples = labeler2.getExamples();
+    console.log(util.inspect(examples, true, null, true /* enable colors */));
+    // 
+    // Remove Example
+    //
+    labeler2.removeExample(example3);
+    examples = labeler2.getExamples();
+    console.log(util.inspect(examples, true, null, true /* enable colors */));
+
+    //
+    // Get Labels
+    //
+    var labels = labeler2.getLabels();
+    console.log(util.inspect(labels, true, null, true /* enable colors */));
+
   }
 }
