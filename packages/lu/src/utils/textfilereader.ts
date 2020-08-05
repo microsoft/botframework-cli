8f1d0b317370a5531117a5c6adc809e1c943177a
Vishwac Sena Kannan <vkannan@microsoft.com>
2020-08-05 13:40:35 -0700

    fixing linting issues.

--- packages/lu/src/utils/textfilereader.ts
+++ packages/lu/src/utils/textfilereader.ts
@@ -6,7 +6,7 @@
 const fs = require('fs-extra')
 const error = require('./../parser/utils/exception')
 const retCode = require('./../parser/utils/enums/CLI-errors')
-const helpers = require('./../parser/utils/helpers');
+const helpers = require('./../parser/utils/helpers')
 
 export async function readTextFile(file: any): Promise<string> {
   return new Promise(async (resolve, reject) => {
@@ -15,7 +15,7 @@ export async function readTextFile(file: any): Promise<string> {
         return reject('ENOENT: no such file or directory, ' + file)
       }
       let fileBuffer = await fs.readFile(file)
-      return resolve(helpers.fixBuffer(fileBuffer));
+      return resolve(helpers.fixBuffer(fileBuffer))
     } catch (err) {
       if (err.message.match(/ENOENT: no such file or directory/)) {
         return reject(new error(retCode.errorCode.INVALID_INPUT_FILE, err.message))
@@ -25,4 +25,3 @@ export async function readTextFile(file: any): Promise<string> {
     }
   })
 }
-
