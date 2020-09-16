69ea5d08cfa50f021faa08f5ea1b5f5b749c4f69
Chris McConnell <chrimc@microsoft.com>
2020-09-16 15:06:23 -0700

    Add --imports switch and improve exported assets copying (#980)
    * Add --imports switch.
    Copy whole sub-directory of ExportedAssets including npm.
    
    * Always put in ImportedAssets
    
    * Ensure uischema keys are sorted.
    
    Co-authored-by: Emilio Munoz <emmunozp@microsoft.com>

--- packages/dialog/src/commands/dialog/merge.ts
+++ packages/dialog/src/commands/dialog/merge.ts
@@ -7,7 +7,7 @@ import {Command, flags} from '@microsoft/bf-cli-command'
 import SchemaMerger from '../../library/schemaMerger'
 
 export default class DialogMerge extends Command {
-    static description = 'Merge `<kind>.schema` and `<kind>[.<locale>].uischema` definitions from a project and its dependencies into a single .schema for describing .dialog files and a per locale .uischema for describing how Composer shows them.  For C#, ensures all nuget declarative resources in ExportedAssets are copied to ImportedAssets in the same location.'
+    static description = 'Merge `<kind>.schema` and `<kind>[.<locale>].uischema` definitions from a project and its dependencies into a single .schema for describing .dialog files and a per locale .uischema for describing how Composer shows them.  If a dependent package has an ExportedAssets directory it is copied to ImportedAssets/<package> in the --imports directory.'
 
     static args = [
         {name: 'patterns', required: true, description: 'Any number of glob regex patterns to match .csproj, .nuspec or package.json files.'},
@@ -20,6 +20,7 @@ export default class DialogMerge extends Command {
         extension: flags.string({description: 'Extension to include as a resource for C#.', required: false, multiple: true, default: ['.dialog', '.lg', '.lu', '.schema', '.qna', '.uischema']}),
         help: flags.help({char: 'h'}),
         nugetRoot: flags.string({description: 'Nuget root directory for debugging.', hidden: true}),
+        imports: flags.string({description: 'Output path for imported assets.  Defaults to the directory of --out with an ImportedAssets directory.', required: false}),
         output: flags.string({char: 'o', description: 'Output path and filename for merged .schema and .uischema.  Defaults to first project name.', required: false}),
         schema: flags.string({char: 's', description: 'Path to merged .schema file to use if merging .uischema only.', required: false}),
         verbose: flags.boolean({char: 'v', description: 'Show verbose logging of files as they are processed.', default: false}),
@@ -32,7 +33,7 @@ export default class DialogMerge extends Command {
 
     async run() {
         const {argv, flags} = this.parse(DialogMerge)
-        let merger = new SchemaMerger(argv, flags.output, flags.verbose, this.log, this.warn, this.error, flags.extension, flags.schema, flags.debug, flags.nugetRoot)
+        let merger = new SchemaMerger(argv, flags.output, flags.imports, flags.verbose, this.log, this.warn, this.error, flags.extension, flags.schema, flags.debug, flags.nugetRoot)
         await merger.merge()
     }
 }
