ce6aa6ce600f71998ce2dbcd3041310fe1b37e1a
Tien Suwandy <tiens@microsoft.com>
2020-10-13 16:31:15 -0700

    Update build.test.ts

--- packages/orchestratorlib/test/build.test.ts
+++ packages/orchestratorlib/test/build.test.ts
@@ -2,31 +2,35 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import {LabelResolver} from '../src/labelresolver';
 import {OrchestratorHelper} from '../src/orchestratorhelper';
 import {OrchestratorBuild} from '../src/build';
+import {OrchestratorNlr} from '../src/nlr';
+import {Utility} from '../src/utility';
+import {UnitTestHelper} from './utility.test';
+
+import assert = require('assert');
 import * as path from 'path';
-const sinon: any = require('sinon');
+const nlrId: string = 'pretrained.20200924.microsoft.dte.00.03.en.onnx';
+const nlrPath: string = path.resolve('./resources/model/model_dte_bert_3l');
 
 describe('OrchestratorBuildTests', () => {
-  beforeEach(() => {
-    const snapshot: Uint8Array = OrchestratorHelper.getSnapshotFromFile(path.resolve('./test/fixtures/dispatch/orchestrator.blu'));
-    sinon.stub(LabelResolver, 'loadNlrAsync');
-    sinon.stub(LabelResolver, 'addExamples');
-    sinon.stub(LabelResolver, 'createSnapshot').returns(snapshot);
-  });
-
-  afterEach(() => {
-    sinon.restore();
+  beforeEach(async () => {
+    Utility.debuggingLog('Downloading an NLR model for unit test');
+    await UnitTestHelper.downloadModelFileForTest(
+      nlrId,
+      nlrPath,
+      OrchestratorNlr.defaultHandler,
+      OrchestratorNlr.defaultHandler);
   });
 
   it('runAsync', async () => {
-    /*
-    await OrchestratorBuild.runAsync(
-      './test/fixtures/',
-      OrchestratorHelper.getLuInputs('./test/fixtures/dispatch/'),
+    const retPayload: any = await OrchestratorBuild.runAsync(
+      nlrPath,
+      OrchestratorHelper.getLuInputs('./test/fixtures/adaptive/'),
       true,
       JSON.parse(OrchestratorHelper.readFile('./test/fixtures/luConfig.json')));
-    */
+
+    assert.ok(retPayload !== null);
+    assert.ok(retPayload.outputs !== null);
   });
 });
