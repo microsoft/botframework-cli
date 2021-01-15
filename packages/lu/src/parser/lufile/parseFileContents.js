2e9729c4628fc08ea494cec99ef98d65183b973d
Fei Chen <feich@microsoft.com>
2021-01-15 15:45:53 +0800

    fix entity type case sensitive issue and a typo

--- packages/lu/src/parser/lufile/parseFileContents.js
+++ packages/lu/src/parser/lufile/parseFileContents.js
@@ -1109,7 +1109,7 @@ const parseAndHandleSimpleIntentSection = function (parsedContent, luResource, c
                         if (hashTable[uttHash]) {
                             utteranceObject = hashTable[uttHash];
                         } else {
-                            utteranceObject = new helperClass.uttereances(utterance, intentName, []);
+                            utteranceObject = new helperClass.utterances(utterance, intentName, []);
                             parsedContent.LUISJsonStructure.utterances.push(utteranceObject);
                             hashTable[uttHash] = utteranceObject;
                         }
@@ -1187,7 +1187,7 @@ const parseAndHandleSimpleIntentSection = function (parsedContent, luResource, c
                         }
                     } else {
                         if(!hashTable[uttHash]) {
-                            let utteranceObject = new helperClass.uttereances(utterance, intentName, []);
+                            let utteranceObject = new helperClass.utterances(utterance, intentName, []);
                             parsedContent.LUISJsonStructure.utterances.push(utteranceObject);
                             hashTable[uttHash] = utteranceObject;
                         }
