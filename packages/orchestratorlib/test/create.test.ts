116dcad48a2cc63e9082b32571767fe31833d409
Tien Suwandy <tiens@microsoft.com>
2021-02-09 12:10:33 -0800

    updated pnpm-lock file

--- packages/orchestratorlib/test/create.test.ts
+++ packages/orchestratorlib/test/create.test.ts
@@ -12,9 +12,8 @@ import {OrchestratorHelper} from '../src/orchestratorhelper';
 import assert = require('assert');
 
 describe('OrchestratorCreateTests', () => {
-  const outputPath: string = './test/fixtures/dispatch';
-
   it('Create Dispatch Snapshot', async function (): Promise<void> {
+    const outputPath: string = './test/fixtures/dispatch';
     Utility.toPrintDebuggingLogToConsole = true;
     this.timeout(UnitTestHelper.getDefaultFunctionalTestTimeout());
     const basemodelId: string = 'pretrained.20200924.microsoft.dte.00.03.en.onnx';
@@ -39,4 +38,32 @@ describe('OrchestratorCreateTests', () => {
 
     assert.ok(Utility.exists(snapshotPath));
   });
+/*
+  it('Create Snapshot with multiple intents', async function (): Promise<void> {
+    const outputPath: string = './test/fixtures/output/multiintent';
+    Utility.toPrintDebuggingLogToConsole = true;
+    this.timeout(UnitTestHelper.getDefaultFunctionalTestTimeout());
+    const basemodelId: string = 'pretrained.20200924.microsoft.dte.00.03.en.onnx';
+    const baseModelPath: string = path.resolve('./resources/model/model_dte_bert_3l');
+    Utility.debuggingLog('Test.0100 OrchestratorTest.runAsync()-Bert-3-layer: downloading a base nerual network language model for unit test');
+    await UnitTestHelper.downloadModelFileForTest(
+      basemodelId,
+      baseModelPath,
+      OrchestratorBaseModel.defaultHandler,
+      OrchestratorBaseModel.defaultHandler);
+
+    const snapshotPath: string = path.join(outputPath, OrchestratorHelper.SnapshotFileName);
+    if (Utility.exists(snapshotPath)) {
+      Utility.deleteFile(snapshotPath);
+    }
+    await OrchestratorCreate.runAsync(
+      baseModelPath,
+      '',
+      outputPath,
+      outputPath,
+      true);
+
+    assert.ok(Utility.exists(snapshotPath));
+  });
+  */
 });
