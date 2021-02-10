c03665fb29598635a8ca4833f6ffb0b49ba4ecc2
Tien Suwandy <tiens@microsoft.com>
2021-02-10 15:53:56 -0800

    Update add.test.ts

--- packages/orchestrator/test/commands/orchestrator/add.test.ts
+++ packages/orchestrator/test/commands/orchestrator/add.test.ts
@@ -12,4 +12,13 @@ describe('orchestrator:add tests', () => {
   .it('should print the help contents when --help is passed as an argument', (ctx: any) => {
     expect(ctx.stdout).to.contain('Add examples from .lu/.qna/.json/.blu files');
   });
+  /*
+  test
+  .stdout()
+  .command(['orchestrator:add', '-t', 'luis', '--id', '8cf0f647-e080-4d79-a54d-4558b3700998', '-k', '8c5c4483797a433dbbdd5b34a82a62ad', '--endpoint', 'https://westus.api.cognitive.microsoft.com'])
+  .exit(1)
+  .it('add luis source', (ctx: any) => {
+    expect(ctx.stdout).to.contain('Added luis source with id 8cf0f647-e080-4d79-a54d-4558b3700998');
+  });
+  */
 });
