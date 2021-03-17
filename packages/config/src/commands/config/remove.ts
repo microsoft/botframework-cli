ae307017111700c6c9e9140dabd414e68a4102d0
Emilio Munoz <emmunozp@microsoft.com>
2021-03-17 01:41:17 -0700

    Adding command config:remove

---
+++ packages/config/src/commands/config/remove.ts
@@ -0,0 +1,26 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {Command, flags} from '@microsoft/bf-cli-command'
+import {getConfigFile, writeConfigFile, Config} from './../../utils/configfilehandler'
+
+export default class ConfigRemove extends Command {
+  static description = 'Removes the specified key from the config file'
+
+  static flags: flags.Input<any> = {
+    help: flags.help({char: 'h'}),
+    key: flags.string({char: 'k', description: 'Name of the key to remove', required: true}),
+  }
+
+  async run() {
+    const {flags} = this.parse(ConfigRemove)
+    let userConfig: Config = await getConfigFile(this.config.configDir)
+    if (userConfig[flags.key]) {
+      delete userConfig[flags.key]
+      await writeConfigFile(this.config.configDir, userConfig)
+    }
+    this.log(`${flags.key} deleted`)
+  }
+}
