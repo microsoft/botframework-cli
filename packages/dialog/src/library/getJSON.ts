df6c1b55a90fe96bc38685f3b936cac3cd7d97b5
cosmicshuai <shuai_uf@outlook.com>
2021-04-14 14:21:50 +0800

    modify https proxy setting

--- packages/dialog/src/library/getJSON.ts
+++ packages/dialog/src/library/getJSON.ts
@@ -5,8 +5,44 @@
 
 import * as fs from 'fs-extra'
 const axios = require('axios')
-const axiosHttpsProxy = require('axios-https-proxy')
-axios.interceptors.request.use(axiosHttpsProxy)
+const url = require('url')
+const httpsProxyAgent = require('https-proxy-agent')
+
+function httpsProxy (config) {
+  /* istanbul ignore if */
+  if (config.socketPath != null) return config
+
+  let parsed = url.parse(config.url)
+  const protocol = parsed.protocol
+  /* istanbul ignore if */
+  if (protocol !== 'https:') return config
+
+  var proxyUrl = process.env['HTTPS_PROXY'] || process.env['https_proxy']
+  /* istanbul ignore if */
+  if (!proxyUrl) return config
+
+  parsed = url.parse(proxyUrl)
+  const proxyOptions = {
+  hostname: parsed.hostname,
+  port: parsed.port,
+  auth: undefined
+  }
+
+  if (parsed.auth) {
+  proxyOptions.auth = parsed.auth
+  }
+
+
+  // HTTPS request must use tunnel proxy protocol
+  config.httpsAgent = httpsProxyAgent(proxyOptions)
+
+  // Disable direct proxy protocol in axios http adapter
+  config.proxy = false
+
+  return config
+}
+
+axios.interceptors.request.use(httpsProxy)
 
 const filePrefix = 'file:///'
 
--- packages/lu/src/parser/qnabuild/serviceBase.js
+++ packages/lu/src/parser/qnabuild/serviceBase.js
@@ -4,10 +4,45 @@
  */
 
 const axios = require('axios')
-const axiosHttpsProxy = require('axios-https-proxy')
+const url = require('url')
+const httpsProxyAgent = require('https-proxy-agent')
 const os = require('os')
 const packageJSON = require('./../../../package')
-axios.interceptors.request.use(axiosHttpsProxy)
+
+function httpsProxy (config) {
+  /* istanbul ignore if */
+  if (config.socketPath != null) return config
+
+  let parsed = url.parse(config.url)
+  const protocol = parsed.protocol
+  /* istanbul ignore if */
+  if (protocol !== 'https:') return config
+
+  const proxyUrl = process.env['HTTPS_PROXY'] || process.env['https_proxy']
+  /* istanbul ignore if */
+  if (!proxyUrl) return config
+
+  parsed = url.parse(proxyUrl)
+  const proxyOptions = {
+  hostname: parsed.hostname,
+  port: parsed.port
+  }
+  if (parsed.auth) {
+  proxyOptions.auth = parsed.auth
+  }
+
+
+  // HTTPS request must use tunnel proxy protocol
+  config.httpsAgent = httpsProxyAgent(proxyOptions)
+
+  // Disable direct proxy protocol in axios http adapter
+  config.proxy = false
+
+  return config
+}
+
+axios.interceptors.request.use(httpsProxy)
+
 
 /**
  * Base class for all services
