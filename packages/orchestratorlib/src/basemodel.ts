1758a7c75ad81bce3ffbe936e14aeae09b309ef7
Tien Suwandy <tiens@microsoft.com>
2021-04-14 10:24:55 -0700

    node-fetch-with-proxy changes

--- packages/orchestratorlib/src/basemodel.ts
+++ packages/orchestratorlib/src/basemodel.ts
@@ -7,7 +7,7 @@ import * as fs from 'fs-extra';
 import * as path from 'path';
 import {Utility} from './utility';
 const unzip: any = require('unzip-stream');
-const fetch: any = require('node-fetch');
+const fetch: any = require('node-fetch-with-proxy');
 
 export class OrchestratorBaseModel {
   public static NlrVersionsSchema: string = '0.2';
