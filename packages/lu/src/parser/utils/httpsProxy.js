6b521a4bcda3e407ba72ded239706d71c9c70655
cosmicshuai <shuai_uf@outlook.com>
2021-04-14 15:05:41 +0800

    fix lint

--- packages/lu/src/parser/utils/httpsProxy.js
+++ packages/lu/src/parser/utils/httpsProxy.js
@@ -7,33 +7,28 @@ const url = require('url')
 const httpsProxyAgent = require('https-proxy-agent')
 
 const httpsProxy = function(config) {
-  /* istanbul ignore if */
-  if (config.socketPath != null) return config
-
-  let parsed = url.parse(config.url)
+  const parsed = url.parse(config.url)
   const protocol = parsed.protocol
-  /* istanbul ignore if */
-  if (protocol !== 'https:') return config
-
-  const proxyUrl = process.env['HTTPS_PROXY'] || process.env['https_proxy']
-  /* istanbul ignore if */
-  if (!proxyUrl) return config
-
-  parsed = url.parse(proxyUrl)
-  const proxyOptions = {
-  hostname: parsed.hostname,
-  port: parsed.port
-  }
-  if (parsed.auth) {
-  proxyOptions.auth = parsed.auth
+  if (protocol !== 'https:') {
+      return config
   }
 
+  const envProxy = process.env['HTTPS_PROXY'] || process.env['https_proxy']
+  if (envProxy) {
+    const parsed = url.parse(envProxy)
+    const proxyOpt = {
+    hostname: parsed.hostname,
+    port: parsed.port
+    }
 
-  // HTTPS request must use tunnel proxy protocol
-  config.httpsAgent = httpsProxyAgent(proxyOptions)
-
-  // Disable direct proxy protocol in axios http adapter
-  config.proxy = false
+    if (parsed.auth) {
+    proxyOpt.auth = parsed.auth
+    }
+    
+    config.httpsAgent = httpsProxyAgent(proxyOpt)
+    //Disable direct proxy
+    config.proxy = false
+  }
 
   return config
 }
