b68865298a2ba979448a0202246bd8b7be77d70e
Tien Suwandy <tiens@microsoft.com>
2021-09-16 17:00:06 -0700

    Surface lu parser error  (#1291)
    * surface parsing error to bf cli users
    
    * added tests
    
    * fixed lint errors
    
    * Update orchestratorhelper.ts
    
    * Update orchestratorhelper.ts
    
    fixed lint error

--- packages/orchestratorlib/test/orchestratorhelper.test.ts
+++ packages/orchestratorlib/test/orchestratorhelper.test.ts
@@ -425,7 +425,6 @@ describe('Test Suite - orchestratorhelper', () => {
     assert.ok((utteranceEntityLabelsMap.get(utterance) as Label[]).length === 3,
       `(utteranceEntityLabelsMap.get(utterance) as Label[]).length=${(utteranceEntityLabelsMap.get(utterance) as Label[]).length}`);
   });
-
   it('Test.0300 OrchestratorHelper.getJsonIntentEntityScoresUtterances()', function () {
     Utility.resetFlagToPrintDebuggingLogToConsole(UnitTestHelper.getDefaultUnitTestDebuggingLogFlag());
     this.timeout(UnitTestHelper.getDefaultUnitTestTimeout());
@@ -587,5 +586,55 @@ describe('Test Suite - orchestratorhelper', () => {
     assert.ok(result.has('Hey'), 'Incorrect result from getUtteranceLabelsMap, missing Hey utterance');
     assert.ok(result.has('Add item'), 'Incorrect result from getUtteranceLabelsMap, missing Add item utterance');
     assert.ok(result.has('delete to do go shopping'), 'Incorrect result from getUtteranceLabelsMap, missing delete to do go shopping utterance');
+  });
+  it('Test.0600 OrchestratorHelper.parseQnaFile()', async () => {
+    const validFile: string = './test/fixtures/parser/valid.qna';
+    const inValidFile: string = './test/fixtures/parser/invalid.qna';
+    const utteranceLabelsMap: Map<string, Set<string>> = new Map<string, Set<string>>();
+    const utteranceLabelDuplicateMap: Map<string, Set<string>> = new Map<string, Set<string>>();
+    try {
+      await OrchestratorHelper.parseQnaFile(
+        inValidFile,
+        '',
+        utteranceLabelsMap,
+        utteranceLabelDuplicateMap);
+      assert.fail('Invalid syntax exception is not thrown.');
+    } catch {}
+    assert.ok(utteranceLabelsMap.size === 0);
+    await OrchestratorHelper.parseQnaFile(
+      validFile,
+      '',
+      utteranceLabelsMap,
+      utteranceLabelDuplicateMap);
+    assert.ok(utteranceLabelsMap.size > 0);
+  });
+  it('Test.0600 OrchestratorHelper.parseLuFile()', async () => {
+    const validFile: string = './test/fixtures/parser/valid.lu';
+    const inValidFile: string = './test/fixtures/parser/invalid.lu';
+    const utteranceLabelsMap: Map<string, Set<string>> = new Map<string, Set<string>>();
+    const utteranceLabelDuplicateMap: Map<string, Set<string>> = new Map<string, Set<string>>();
+    const utteranceEntityLabelsMap: Map<string, Label[]> = new Map<string, Label[]>();
+    const utteranceEntityLabelDuplicateMap: Map<string, Label[]> = new Map<string, Label[]>();
+
+    try {
+      await OrchestratorHelper.parseLuFile(
+        inValidFile,
+        '',
+        utteranceLabelsMap,
+        utteranceLabelDuplicateMap,
+        utteranceEntityLabelsMap,
+        utteranceEntityLabelDuplicateMap);
+      assert.fail('Invalid syntax exception is not thrown.');
+    } catch {}
+    assert.ok(utteranceLabelsMap.size === 0);
+
+    await OrchestratorHelper.parseLuFile(
+      validFile,
+      '',
+      utteranceLabelsMap,
+      utteranceLabelDuplicateMap,
+      utteranceEntityLabelsMap,
+      utteranceEntityLabelDuplicateMap);
+    assert.ok(utteranceLabelsMap.size > 0);
   });
 });
